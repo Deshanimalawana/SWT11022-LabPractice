f686460b3b59df059db10fcb318e29e95ae5970f
RMPMRathnayake <rathnayakam021@gmail.com>
2025-02-20 15:22:12 +0530

    Add files via upload

---
+++ Lab_02_task_03.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    // Variable declaration
+    char name[20];
+    int a, b;
+    float c, f;
+    // Variable initialization
+    char val[] = "Welcome";
+    a = 10;
+    // Defining Constants
+    const float x = 3.73;
+    // Interaction with the user
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("%s", val);
+    printf("\nYour name is %s.", name);
+    c = 90.0 * 3.0;
+    printf("\nValue of c: %f", c);
+    printf("\nValue of X: %f", x);
+    // Using escape sequences
+    printf("\nHello\tWorld\n\n");
+    printf("You\nare\nlearning\n'c' language\n\"Do you know C language\"");
+    return 0;
+}
