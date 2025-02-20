6984a32ad0b05dc4678a8ce6e2f4607076eb78ee
ThinethAdheesha <rataadeesha@gmail.com>
2025-02-20 13:53:58 +0530

    Add files via upload

---
+++ Task1 .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Variable declaration and initialization
+    int id = 101;         // Integer data type
+    char section = 'B';   // Character data type
+    float marks = 89.5;   // Floating-point data type
+
+    // Printing the output using printf()
+    printf("ID: %d, Section: %c, Marks: %.2f\n", id, section, marks);
+
+    return 0;
+}
