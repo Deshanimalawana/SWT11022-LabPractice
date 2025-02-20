90a830b467ee2ba5c6328290a977dc2a6907d017
vihagi-2003 <pulsarabalasoriya@gmail.com>
2025-02-20 11:12:29 +0530

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+int marks=65;
+char grade='A';
+float weight= 25.5;
+
+printf("Your name:%d\n",marks);
+printf("Your grade:%c\n",grade);
+printf("Your weight:%f\n",weight);
+
+return 0;
+
+}
---
+++ Untitled5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+float price= 59.99;
+int quantity= 4;
+float total= price* quantity;
+
+printf("Total is:%f",total);
+
+return 0;
+
+}
