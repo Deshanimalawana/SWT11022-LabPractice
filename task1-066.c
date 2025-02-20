a5b03280d0c040a3ac0ba9303c16c36a53534be4
Apinesh <apineshtharmarasa105@gmail.com>
2025-02-20 11:34:11 +0530

    Uploaded files - by SEU/IS/22/ICT/066

---
+++ task1-066.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int reg = 55;
+    float temp = 65.25;
+    char grade = 'A';
+
+    printf("Registration No : %d\nTemperature : %.2f\nGrade : %c", reg, temp, grade);
+
+    return 0;
+}
