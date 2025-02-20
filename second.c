6c0c236cb032b328bd710a18c5cf6d1734390141
Arosha Chirath <aroshachirath@gmail.com>
2025-02-20 11:06:58 +0530

    Add files via upload

--- second.c
+++ second.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+int main (){
 
-int main() {
-    float hours , payment , price;
-    printf("hours");
-    scanf("%f",&hours);
-    printf("payment_for_one_hours");
-    scanf("%f",&payment);
-    price = hours*payment;
-    printf("price=%f", price);
-return 0;
+ float doller = 59.9;
+ int quantity = 4;
+ float total = doller*quantity;
+
+ printf("Total price:%.2f\n",total);
+ return 0;
 }
