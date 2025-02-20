37c4d135f8d0e2f656c0603bbd4dbbcf41c34d96
lakshanシs <lakshanpersonal1@gmail.com>
2025-02-20 14:16:23 +0530

    Add files via upload

---
+++ task1q3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    char name[20];
+    int a, b;
+    float c, f;
+    char val[] = "Welcome"; a= 10;
+    const float x = 3.73;
+
+    printf("Enter your name: ");
+    scanf("%s", &name);
+    printf(val);
+    printf("\nYour name is %s.", name);
+    c= 90.0*3.0;
+    printf("\nValue of c: %f", c);
+    printf("\nValue of b: %f", b);
+    printf("Hello\tWorld\n\n");
+    printf("You\nare\nlearning\n\'c\' language\n\"Do you know C language\"");
+
+return 0;
+}
