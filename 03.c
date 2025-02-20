721148ed1fa06a2a0b63342ed8839c1df906fdca
dinuka1202 <166311813+dinuka1202@users.noreply.github.com>
2025-02-20 11:21:56 +0530

    Add files via upload

---
+++ 03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+
+float payRate;
+int hours;
+printf("Enter totel worked hours");
+scanf("%d",&hours);
+printf("Enter pay rate:");
+
+scanf("%f", &payRate);
+printf("Your pay Rate: %2f, worked hours :%d\n",payRate,hours);
+return 0;
+
+}
