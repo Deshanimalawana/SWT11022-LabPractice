d3ec83e965e63c22e862a0bc02f58475e1733250
Dhusanthika <thusanthika2003@gmail.com>
2025-02-20 14:22:17 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
 
-    int id = 101;
-    char section ='B';
-    float marks = 89.5;
-    printf("ID: %d, Section: %c, Marks: %2f\n", id, section, marks);
+int main()
+{
+    int hours;
+    float payRate;
+    printf("How many hours do yo work: ");
+    scanf("%d",&hours);
+    printf("pay rate hours: ");
+    scanf("%f", &payRate);
+    float salary=payRate*hours;
+    printf("your pay rate:%.2f, Worked Hours:%d\n",payRate,hours);
+    printf("The total salary is :RS%.0f",salary);
     return 0;
 }
