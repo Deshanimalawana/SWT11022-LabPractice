a299c65259a80482113a610be94f3ed31e4beef3
1NazmiAhamed <ahamednaazmi@gmail.com>
2025-02-20 11:38:44 +0530

    Update 3_96.c

--- 3_96.c
+++ 3_96.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 int main() {
-    float pricePerKg = 102.99;
-    int quantity = 1000;
-    float totalCost;
+    float price_Per_Kilo= 900.454;
+    int Tot_quantity=26000;
+    float total_Cost;
 
-    totalCost = pricePerKg * quantity;
+    total_Cost = price_Per_Kilo * Tot_quantity;
 
-    printf("Rice price: %.2f per kg\n", pricePerKg);
-    printf("Quantity: %d kg\n", quantity);
-    printf("Total cost: %.2f\n", totalCost);
+    printf("Rice price: %.2f per kg\n", price_Per_Kilo);
+    printf("Quantity: %d kg\n", Tot_quantity);
+    printf("Total cost: %.2f\n", total_Cost);
 
     return 0;
 }
