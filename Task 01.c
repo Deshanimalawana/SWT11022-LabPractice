08dd5004fc6150b304f8898701ea99aba28ba50f
Rushdhy18 <rushdhyhaneem18@gmail.com>
2025-02-20 11:12:57 +0530

    Add files via upload

---
+++ Task 01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int id = 101;
+    char section = 'B';
+    float marks = 89.5;
+
+    printf("ID:%d\n",id);
+    printf("Section:%c\n",section);
+    printf("Marks:%.2f\n",marks);
+
+
+
+
+
+    return 0;
+}
+
