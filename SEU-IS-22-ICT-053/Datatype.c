2cdfaa896390e10aaf2d6d2cd213aecaa7c9ed36
lakshanderren <isuru21256@gmail.com>
2025-02-20 11:52:42 +0530

    change datatype file

---
+++ SEU-IS-22-ICT-053/Datatype.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+
+int integer = 101;
+char character = 'B';
+float floatValue = 89.5;
+double doubleValue = 3.333333;
+printf(" Integer value is %integer\n character value is %character\n character value is %character\n float value is %floatValue\n double value is %doubleValue\n",integer,character,floatValue,doubleValue);
+
+
+}
