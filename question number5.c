2a8613be3287673797fc3e765ceb37d10c4ac7ae
vihagi-2003 <pulsarabalasoriya@gmail.com>
2025-02-20 15:32:23 +0530

    Add files via upload

---
+++ question number5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    float length = 25.12428, width = 12.59, area;
+
+
+    area = length * width;
+
+
+    printf("The area of the rectangle is: %.4f cm^2\n", area);
+
+    return 0;
+}
