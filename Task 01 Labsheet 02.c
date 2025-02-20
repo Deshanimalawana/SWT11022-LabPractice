b3a397e3cd841f2fdc8fe563dfbdc4858ae7625c
Kularasan Jelaxsi <rajanjela22@gmail.com>
2025-02-20 11:24:31 +0530

    Add files via upload

---
+++ Task 01 Labsheet 02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int id = 101;
+   char section ='B';
+   float marks =89.5;
+
+   printf("ID:%d\nsection:%c\nMarks:%f\n",id,section,marks);
+
+   return 0;
+}
