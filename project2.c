154b1299370bcffd4cc8e102de2c17830cabdae4
hafrathusam <nuhafrath2023@gmail.com>
2025-02-20 11:51:42 +0530

    Add files via upload

---
+++ project2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main() {
+
+    float pricePerKg = 102.99;
+    int quantity = 1000;
+    float totalCost;
+
+    totalCost = pricePerKg * quantity;
+
+
+    printf("Rice price: %.2f per kg\n", pricePerKg);
+    printf("Quantity: %d kg\n", quantity);
+    printf("Total cost: %.2f\n", totalCost);
+
+    return 0;
+}
