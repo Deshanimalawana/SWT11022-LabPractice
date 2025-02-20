6011108c3457df23f74a2e1e32e317a9ea507934
Hasini110 <hasinitheekshana48@gmail.com>
2025-02-20 12:26:00 +0530

    Add files via upload

---
+++ Q.03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+
+    float payRate ;
+    int hours ;
+
+    printf("Enter total worked hours:");
+    scanf("%d", &hours);
+    printf("Enter pay rate: ");
+    scanf("%f", &payrate);
+    int salary=payrate*hours;
+    printf("salary,%d",salary);
+
+    return 0;
+}
