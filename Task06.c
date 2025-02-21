3c7790a5e82f4df3aa30cdce79dffcff88391bbd
mihinavo <mihiranganavodya@gmail.com>
2025-02-21 23:28:19 +0530

    Add files via upload

---
+++ Task06.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    double length = 25;
+    double width = 10;
+    double area = length * width;
+    printf("The area of the rectangle is: %.4f cm²\n", area);
+
+    return 0;
+}
+
