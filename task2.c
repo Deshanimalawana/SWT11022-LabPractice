c2acc85b483b62937f0025db68661087080723ba
shabrafathi <shabrasanoor@gmail.com>
2025-02-22 20:04:44 +0530

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-int main(){
 
-    float payRate;
-    int hours;
-    printf("enter total worked hours: ");
-    scanf("%d", &hours);
-    printf("enter pay rate: ");
-    scanf("%f", &payRate);
-    printf("your pay rate: %.2f,worked hours:%d\n",payRate,hours);
+int main() {
+    float hours, rate, salary;
+
+    printf("Enter total worked hours: ");
+    scanf("%f", &hours);
+
+    printf("Enter pay rate per hour: ");
+    scanf("%f", &rate);
+
+    salary = hours * rate;
+    printf("Total salary: %.2f\n", salary);
+
     return 0;
-
 }
