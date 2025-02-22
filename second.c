da4222fcb92a104e4b02891e645c8c1c06fe0e01
miabdulrahman <m.i.abdulrahmanar@gmail.com>
2025-02-22 17:06:11 +0530

    updated

--- second.c
+++ second.c
@@ -1,14 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-int main() {
-    float hours , payment , price;
-    printf("hours");
-    scanf("%f",&hours);
-    printf("payment_for_one_hours");
-    scanf("%f",&payment);
-    price = hours*payment;
-    printf("price=%f", price);
+include <stdio.h>
+int main(void)
+{
+  prntf("Welcome to the fundamentals of programing Session\n");
 return 0;
 }
