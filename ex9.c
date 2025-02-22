f27932c449b4f3295eefe02bb9cc5acdbc7070ad
Dhusanthika <thusanthika2003@gmail.com>
2025-02-22 12:42:14 +0530

    Add files via upload

---
+++ ex9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Declare characters
+    char a = 'X';
+    char b = 'M';
+    char c = 'L';
+
+    // Print the desired output
+    printf("The reverse of %c%c%c is %c%c%c\n", a, b, c, c, b, a);
+
+    return 0;
+}
