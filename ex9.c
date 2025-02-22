9ffa0fd8b7118353f3d562afe54765aa63a27610
Dhusanthika <thusanthika2003@gmail.com>
2025-02-22 12:50:34 +0530

    Update ex9.c

--- ex9.c
+++ ex9.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    // Declare characters
+    
     char a = 'X';
     char b = 'M';
     char c = 'L';
 
-    // Print the desired output
+    
     printf("The reverse of %c%c%c is %c%c%c\n", a, b, c, c, b, a);
 
     return 0;
