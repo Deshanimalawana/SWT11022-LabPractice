b8303b53d4f74080d9fb1b100415898c5eb61ef6
alfmohammed <157476729+alfmohammed@users.noreply.github.com>
2025-02-22 20:53:45 +0530

    Add files via upload

---
+++ Task 05-077.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    double length = 25.12428;
+    double width = 12.59;
+    double area;
+    area = length * width;
+    printf("Area of a Rectangle: %.4f cm²\n", area);
+    return 0;
+}
