e2108a3e63e17d0968f95af8f55ff9d7b4c2262b
alfmohammed <157476729+alfmohammed@users.noreply.github.com>
2025-02-23 14:01:18 +0530

    Add files via upload

--- Task 04-077.c
+++ Task 04-077.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+int main()
+{
+    char Firstname[60];
+    char Lastname[60];
+    int BirthYear;
 
-  struct Book {
-  char title[100];
-  char author[50];
-  double price;
-  };
-
-  int main() {
-  struct Book myBook;
-
-  strcpy(myBook.title, "The Catcher in the Rye");
-  strcpy(myBook.author, "J.D. Salinger");
-  myBook.price = 30.00;
-
-  printf("Book Title:%s\n",myBook.title);
-  printf("Author:%s\n",myBook.author);
-  printf("Price:$%.2f\n",myBook.price);
-  return 0;
+    printf("What is your Firstname? ");
+    scanf("%s", Firstname);
+    printf("What is your Lastname? ");
+    scanf("%s", Lastname);
+    printf("What is your Birth year? ");
+    scanf("%d", &BirthYear);
+    printf("%s %s was born in %d\n", Firstname, Lastname, BirthYear);
+    return 0;
 }
