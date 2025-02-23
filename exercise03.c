1e7fb08dde4e86b6b7b662e41cb72bff5edd784e
Isuru sampath fernando <isurusampath2322@gmail.com>
2025-02-23 22:11:14 +0530

    Update exercise03.c

--- exercise03.c
+++ exercise03.c
@@ -7,5 +7,8 @@ int main(){
     printf("Enter pay rate: ");
     scanf("%f",&payrate);
     printf("Your pay Rate: %2f, Worked hours: %d\n",payrate,hours);
+
+    float total = payrate*hours;
+    printf("Your total salary: %2f\n",total);
     return 0;
 }
