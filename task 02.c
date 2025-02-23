09c44bc8f22d740243d6e90590cea2bf48fb7cd7
Niranjana Herath <133478917+Niranjana7777@users.noreply.github.com>
2025-02-24 00:17:44 +0530

    Add files via upload

---
+++ task 02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+  float payrate;
+  int hours;
+  float totalsalary;
+  printf("enter total worked hours:");
+  scanf("%d", &hours);
+  printf("enter pay rate:");
+  scanf("%f",&payrate);
+
+  totalsalary = hours*payrate;
+  printf("total salary: %.2f\n",totalsalary);
+  return 0;
+}
