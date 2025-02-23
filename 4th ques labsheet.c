c13474032d3bfef4624cf2c0924bfe16e6007d9c
Rukshana0924 <rukshlath2002@gmail.com>
2025-02-23 12:08:07 +0530

    Add files via upload

---
+++ 4th ques labsheet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char firstname[20],lastname[20];
+    int year;
+printf("What is your first name?");
+scanf("%s",firstname);
+printf("What is your last name?");
+scanf("%s",lastname);
+printf("what is your birth year?");
+scanf("%d",&year);
+printf("%s %s was born in %d",firstname,lastname,year);
+return 0;
+}
---
+++ 5th ques labsheet.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+double length=25.12428,width=12.59;
+float area;
+area=length*width;
+printf("%.4f",area);
+return 0;
+}
