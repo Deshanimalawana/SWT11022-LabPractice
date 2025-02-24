2f50be538e220d848a47542a59e9b24ff28a787c
kaveeja-042 <kaveejarathnakoon2002@gmail.com>
2025-02-24 23:14:04 +0530

    Add files via upload

--- task 1.c
+++ task 1.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-int main (){
-   int id=101;
-   char section='B';
-   float marks=99.5;
-     printf("Id:%d, section:%c, Marks:%.2f\n",id,section,marks);
-    return 0;
+int main() {
+
+   int id = 101;
+   char section = 'B';
+   float marks = 89.5;
+
+   printf("ID:%d,Section:%c,Marks:%.2f\n",id,section,marks);
+   return 0;
 }
+
--- task 2.c
+++ task 2.c
@@ -1,14 +1,15 @@
-# include <stdio.h>
-int main () {
-    float payrate;
-    int hours;
-    float salary;
+#include <stdio.h>
+int main() {
 
-    printf("Enter total worked hours:");
-    scanf("%d", &hours);
-    printf("Enter pay rate:");
-    scanf("%f", &payrate);
-    salary=hours*payrate;
-    printf("your pay rate:%.2f,worked hours:%d,salary:%.2f\n",payrate,hours,salary);
-    return 0;
+   float Payrate;
+   int hours;
+   printf("Enter total worked hours: ");
+   scanf("%d", &hours);
+   printf("Enter Pay rate: ");
+   scanf("%f", &Payrate);
+   printf("Your Pay rate: %.2f, worked hours: %d\n", Payrate, hours);
+   return 0;
+
 }
+
+
