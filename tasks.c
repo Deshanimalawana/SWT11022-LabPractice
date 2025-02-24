c318167bd5c650ccf0eeb1613a6800af302606c1
Moksha104 <mokshamadurasingha2023@gmail.com>
2025-02-24 22:40:28 +0530

    Add files via upload

---
+++ tasks.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main(){
+    int id=101;
+    char section='B';
+    float marks=89.5;
+    printf("Id: %d,sections: %c,Marks: %C,Marks: %2f\n" ,id,section,marks);
+
+    return 0;
+
+
+ }
--- tasks5.c
+++ tasks5.c
@@ -4,14 +4,26 @@ int main(){
     char lastName[30];
     int birthYear;
 
-    printf("What is your first name?");
+    printf("what is your first name?");
     scanf("%s", firstName);
-    printf("What is your last name?");
+    printf("what is your last name?");
     scanf("%s", lastName);
-    printf("What is your birth year?");
+    printf("what is your birth year?");
     scanf("%d", &birthYear);
     printf("%s %s was born in %d\n", firstName, lastName, birthYear);
 
     return 0;
 
-}
+
+    }
+
+
+
+
+
+
+
+
+
+
+
