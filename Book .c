2cd16b878574655dea9a49b55498779d7ccd0536
jelaxsi <rajanjela22@gmail.com>
2025-02-24 23:39:57 +0530

    New files added

---
+++ Book .c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+    struct Book {
+    char title[100];
+    char author[100];
+    float price;
+
+};
+    int main() {
+    struct Book mybook;
+
+
+    strcpy(mybook.title, "Never Lie");
+    strcpy(mybook.author, "Freida McFadden");
+    mybook.price=2500;
+
+    printf("Book Title: %s\n", mybook.title);
+    printf("Author:%s\n", mybook.author);
+    printf("price:%2f\n", mybook.price);
+
+
+    return 0;
+    }
+
+
---
+++ Task 06 Labsheet 02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    printf("The reverse of XML is LMX\n");
+
+
+
+
+
+
+    return 0;
+}
