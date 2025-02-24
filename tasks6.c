bb1d66c2c0be67de3246411500cce8cdee0d9bdd
Moksha104 <mokshamadurasingha2023@gmail.com>
2025-02-24 23:26:00 +0530

    Delete tasks6.c

--- tasks6.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    double length=25.12428;
-    double width=12.59;
-    double area=length*width;
-
-    printf("The area of the rectangle is: %.4f cm2\n" , area);
-
-}
