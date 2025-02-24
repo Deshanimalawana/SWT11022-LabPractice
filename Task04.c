ae6fd729574bfcecaf78cb59141c15cc099b040a
Thariduj <tharidujayawrdana@gmail.com>
2025-02-24 13:03:49 +0530

    Add Tasks

--- Task04.c
+++ Task04.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    char firstName[100];
-    char lastName[100];
+int main() {
+    char firstname[20];
+    char lastname[20];
+    int birthyear;
 
-    printf("What is your firstname? ");
-    scanf("%s",firstName);
-    printf("What is your lastname? ");
-    scanf("%s",lastName);
-    int year;
-    printf("What is your birth year? ");
-    scanf("%d",&year);
+    printf("Enter your first name: ");
+    scanf("%s", firstname);
+    printf("Enter your last name: ");
+    scanf("%s", lastname);
+    printf("Enter your birth year: ");
+    scanf("%d", &birthyear);
+    printf("%s %s was born in %d \n", firstname, lastname, birthyear);
 
-    printf("%s %s was born in %.d\n",firstName,lastName,year);
     return 0;
 }
---
+++ Task05.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    float Length = 25.12428;
+    float Width = 12.59;
+    float Area = Length * Width;
+    printf("Area is %.4f\n",Area);
+
+    return 0;
+}
