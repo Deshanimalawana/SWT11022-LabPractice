a14b8c88564f61e3f47c10e1a7ccf0828bbc179c
Tashmika Jayamanna <tashmikameriyan@gmail.com>
2025-02-24 12:35:04 +0530

    Labsheet2
    task 4,5,6

---
+++ Task_005.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    float length = 25.12428;
+    float width  = 12.59;
+    float area = length * width;
+    printf("Area of rectangle: %.4f\n ",area);
+    return 0;
+
+}
