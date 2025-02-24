e8156538bdebc81b540a1ec90b10af02b170e7c3
AkashiJanethri039 <nishelakashi318@gmail.com>
2025-02-24 22:05:50 +0530

    Add files via upload

---
+++ task6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  int main(){
+      float legnth=25.12428;
+      float width=12.59;
+
+      float area=legnth*width;
+
+      printf("the area of rectangle is: %.4f cm2\n", area);
+
+      return 0;
+
+
+  }
---
+++ task7.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    char one='X',two='M',three='L';
+
+    printf("the reverse of%c%c%C is %c%c%c\n", one,two,three,three,two,one);
+
+    return 0;
+}
