df59acaa75ecedfdc7fde5e0447145172eb0763d
eshan <eshansaumaya@gmail.com>
2025-02-24 21:54:05 +0530

    Add files via upload

--- task04.c
+++ task04.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int main(){
 
-    struct Book {
-        char title[100];
-        char author[50];
-        double price;
+int main() {
+    char first_name[20];
+    char last_name[20];  
+    int birth_year;  
 
-};
+    
+    printf("What is your first name? ");  
+    scanf("%s", first_name);  
 
-    struct Book myBook;
-        strcpy(myBook.title,"The University");
-        strcpy(myBook.author,"A.H.Kumar");
-        myBook.price = 66.99;
+    printf("What is your last name? ");  
+    scanf("%s", last_name);  
 
-        printf("Book Title:%s\n",myBook.title);
-        printf("Book Author:%s\n",myBook.author);
-        printf("Price:s%.2f\n",myBook.price);
+    printf("What is your birth day? ");  
+    scanf("%d", &birth_year);  
 
-   return 0;
 
+    printf("\n%s %s was born in %d\n", first_name, last_name, birth_year);  
+
+    return 0;  
 }
---
+++ task05.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+    float length = 25.12428;
+    float width = 12.59;
+    float area = length * width;
+    printf("the area of the rectangle : %.4f cm^2 \n",area);
+    return 0;
+    
+}
