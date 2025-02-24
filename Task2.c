b5a37f6a3ff64915029c6bef7dce0def66ea805f
Maheli88 <limanthamaheli@gmail.com>
2025-02-24 23:51:36 +0530

    Add files via upload

--- Task2.c
+++ Task2.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 int main(){
-int quantity = 4;
-float price = 59.99;
-float total = quantity*price;
-printf("Total price : %2f\n", total);
-return 0;
+    float payRate;
+    int hours;
+    float salary;
+
+    printf("Enter total worked hours:");
+    scanf("%d", &hours);
+    printf("Enter pay rate:");
+    scanf("%f", &payRate);
+    salary = payRate*hours;
+
+    printf("Total salary: %f\n", salary);
+    return 0;
+
+
 }
