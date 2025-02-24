f45daae5923706d76f070996f6e3088802ab9c53
randikaweerasinghe <wms.randika03@gmail.com>
2025-02-24 15:49:59 +0530

    delete unwanted files

--- Lab sheet 2 (task,practicals)/Lab Sheet 02 - Practical 1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
- float price =5.99;
- int quantity =5;
- float total =price*quantity;
- printf("Total Price: %.2f\n",total);
- return 0;
-}
--- Q06.c
+++
@@ -1,8 +0,0 @@
-int main() {
-    printf("The reverse of XML is LMX\n");
-
-    return 0;
-}
-
-
-
