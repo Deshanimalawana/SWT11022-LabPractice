f38092068ff4300048956a40133ab46b7f608818
GayaN948 <gayanudayanga1221@gmail.com>
2025-02-26 00:14:07 +0530

    Add files via upload

---
+++ task 04.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int  main(){
+    char firstName[50],lastName[50];
+    int birthyear;
+
+    printf("what  is  your  first  name?");
+    scanf("%s",firstName);
+    printf("what  is  your  last  name?");
+    scanf("%s",lastName);
+    printf("what  is  your  birthyear?");
+    scanf("%d",&birthyear);
+
+    printf("%s %s was  born  in %d\n",firstName,lastName,birthyear);
+    return 0;
+
+
+
+
+
+
+
+
+}
