b90c5570ea96b43a9ca5c0a6b9fabcca1165a669
xpunsara <punsarawickramasingha4@gmail.com>
2025-02-26 12:39:58 +0530

    upload lab 2 practicals

--- 4.c
+++ 4.c
@@ -1,18 +1,18 @@
-#include <stdio.j>
-int main()
-{
+#include <stdio.h>
+
+int main() {
     struct book {
-    char title[100];
-    char author[50];
-    double price;
+        char title[100];
+        char author[50];
+        double price;
     };
 
-    struct book mybook = ("dragon heart"), "Punsara", 3.01};
+    struct book mybook = {"Dragon Heart", "Punsara", 3.01};
 
-    printf("book details:\n");
-    printf("title:%s\n",mybook.title);
-    printf("author:%s\n",mybook.author);
-    printf("price:%s\n",mybook.price);
+    printf("Book Details:\n");
+    printf("Title: %s\n", mybook.title);
+    printf("Author: %s\n", mybook.author);
+    printf("Price: %.2lf\n", mybook.price);
+
     return 0;
-
 }
---
+++ 5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int  main (){
+    double length = 25.12428;
+    double  width = 12.59;
+    double area = length*width;
+
+    printf( "Area  of  the  rectangle  : %.4f square cm\n",area);
+    return 0;
+}
