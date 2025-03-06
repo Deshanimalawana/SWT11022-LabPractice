48eea7522f2b59168755dd4995fbc0e8863155fb
KaveeshaSewmini <kaveesha3sewmini2003@gmail.com>
2025-03-06 11:12:46 +0530

    Add files via upload

---
+++ Student.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int studentID=12345;
+    float GPA= 3.75;
+    char*grade= "A";
+
+    printf("StudentID: %d\n", studentID);
+    printf("GPA: %.2f\n",GPA);
+    printf("Grade: %s\n", grade);
+
+    studentID= 54321;
+    GPA =3.90;
+    grade= "A+";
+
+    printf("\nUpdate Values:\n");
+    printf("StudentID: %d\n", studentID);
+    printf("GPA: %.2f\n", GPA);
+    printf("Grade: %s\n", grade);
+
+    return 0;
+}
