f3a90d50f4e071a454e67ffcf54000f783e4bac4
DArkSHADOW110 <thilankhewawasam2@gmail.com>
2025-03-06 11:30:14 +0530

    labweek4 practicals

---
+++ labweek4 practical 2.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+
+int count =5;
+count++; //increment
+
+int total = 10;
+total+= 3;//add 3 to total
+
+printf("Updated count: %d\n", count);
+printf("Updated total: %d\n", total);
+return 0;
+}
