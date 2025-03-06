9d524852fa5011807003f4bdb05c9448ddafa780
ImeshChathura <88792235+imeshcha@users.noreply.github.com>
2025-03-06 11:37:48 +0530

    Labsheet 04 practicals added

---
+++ logical.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+
+    int condition1 = 1;
+    int condition2 = 0;
+    int condition3 = 1;
+
+    int result1 = condition1 && condition2;
+    int result2 = condition1 || condition2;
+    int result3 = !condition2;
+
+    printf("result 1: %s\n", result1 ? "true" : "false");
+    printf("result 2: %s\n", result2 ? "true" : "false");
+    printf("result 3: %s\n", result3 ? "true" : "false");
+
+}
