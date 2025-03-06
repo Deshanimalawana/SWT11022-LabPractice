c3408842e7ec73a3ddbf648d63618f27bbc36778
Isuru sampath fernando <isurusampath2322@gmail.com>
2025-03-06 10:03:45 +0530

    Add files via upload

---
+++ Exercise 09.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int studentID = 12345;
+    float GPA = 3.75;
+    char grade = 'A';
+
+    printf("\nStudentID:%d \nGPA:%.2f \nGrade:%c",studentID,GPA,grade);
+    return 0;
+
+}
