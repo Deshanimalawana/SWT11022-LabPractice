79f21b571b5aa997e46ca67b2024bdfd0075db12
majhdbgr <rushdhyhaneem18@gmail.com>
2025-03-06 09:38:18 +0530

    Task02 file path changed

---
+++ Lab02Tasks/Task05.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float length = 25.12428;
+    float width = 12.59;
+
+    float area = length * width;
+
+    printf("Area = %.4f\n",area);
+
+}
--- Task06.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-    char ch[10] = "LMX";
-    printf("The reverse of %s is %c%c%c \n",ch, ch[2], ch[1], ch[0]);
-
-    return 0;
-}
