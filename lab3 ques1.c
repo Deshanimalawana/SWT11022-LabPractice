590277573ac4e5cb702a3254b2ca962e66d10348
Rukshana0924 <rukshlath2002@gmail.com>
2025-03-06 10:17:16 +0530

    Add files via upload

---
+++ lab3 ques1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int studentId=12345;
+float GPA=3.75;
+char grade='A';
+//printf("studentid:%d\nGPA:%f\nGrade:%c\n",studentId,GPA,grade);
+studentId=54321;
+GPA=3.90;
+grade='A+';
+printf("studentid:%d\nGPA:%f\nGrade:%c\n",studentId,GPA,grade);
+return 0;
+}
---
+++ lab3 ques2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define SPEED_OF_LIGHT 299792458
+#define GRAVITY 9.81
+#define PLANCK_CONSTANT 6.626E-34
+int main(){
+const float BOLTZMANN_CONSTANT=1.38E-23;
+const int ABSOLUDE_ZERO=-273;
+printf("value of speed of light:%d\n",SPEED_OF_LIGHT);
+printf("value of gravity:%f\n",GRAVITY);
+printf("value of flank constant:%.e\n",PLANCK_CONSTANT);
+printf("value of boltzmann:%.e\n",BOLTZMANN_CONSTANT);
+printf("value of absolude zero: %d\n",ABSOLUDE_ZERO);
+return 0;
+}
