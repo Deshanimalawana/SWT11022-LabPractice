2306be175e3349453632e6678c0f3b9fc629cfe6
Dhusanthika <thusanthika2003@gmail.com>
2025-03-06 10:25:23 +0530

    Add files via upload

---
+++ practise1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <conio.h>
+int main(){
+int studentID=54321;
+float GPA=3.90;
+char grade[]="A+";
+printf("studentID: %d\n",studentID);
+printf("GPA: %.2f\n",GPA);
+printf("grade: %s\n",grade);
+return 0;
+}
+
+
+
