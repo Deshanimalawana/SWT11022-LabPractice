98d88b949dcab6b1865dc73ca432555a5e902da0
Rashmi-Juli <rashmijuliyange@gmail.com>
2025-03-06 11:09:49 +0530

    Add files via upload

---
+++ Bank.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+ int accountBalance=5000;
+ int account_balance=4500;
+ int secontTransaction=200;
+ int transactionAmount=150;
+ int floatValue=100;
+
+ printf("Bank Transaction Details:\n");
+ printf("Account Balance:%D\n",accountBalance);
+ printf("Secondary Account Balance:%d\n",account_balance);
+ printf("Second Transaction:%d\n",secontTransaction);
+ printf("Transaction Amount:%d/n",transactionAmount);
+ printf("Float Value:%d/n",floatValue);
+
+ return 0;
+ }
