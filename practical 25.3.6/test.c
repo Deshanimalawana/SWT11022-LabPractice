182fcf03dc2f73aee679f9854a4d77d58d058da3
Chamindu Ravishan <chamindurupasinghe@gmail.com>
2025-03-06 12:17:09 +0530

    Add files via upload

---
+++ practical 25.3.6/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ int main()
+  {
+    int studentID;
+    float gpa;
+    char grade;
+   studentID=12345;
+    gpa=3.75;
+   grade='A';
+   printf("studentID:%d\n",studentID);
+   printf("GPA:%.2f\n",gpa);
+    printf("grade:%c\n",grade);
+      studentID=54321;
+      gpa=3.90;
+      grade='A+';
+       printf("\nUpdated student details:\n");
+     printf("studentID:%d\n",studentID);
+      printf("GPA:%.2f\n",gpa);
+       printf("Grade:%c\n",grade);
+
+      return 0;
+  }
