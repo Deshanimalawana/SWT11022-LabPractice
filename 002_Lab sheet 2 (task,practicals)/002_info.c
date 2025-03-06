ca5edc160f2d09057463be34568ee3138bd568da
Shahitthiyan Murali <shaketthiyanmurali@gmail.com>
2025-03-06 16:19:44 +0530

    Files deleted

--- 002_Lab sheet 2 (task,practicals)/002_info.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-	printf("Name - Chamikara!\n");
-	printf("Registration Number - SEU/IS/22/ICT/002/!");
-	
- 	return 0;
-}
--- 007(task6).c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("The reverse of XML is %c%c%c \n",'L','M','X');
-    return 0;
-}
