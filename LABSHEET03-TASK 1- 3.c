f642307ed4c5a591a7bcfebd46c01b9d2ea1c3b5
R.D.T.S.Karunarathne <tharakasandaru30@gmail.com>
2025-03-06 10:10:26 +0530

    Add files via upload

---
+++ LABSHEET03-TASK 1- 3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void){
+
+// 01. valid: customerID, totalAmount, discountRate
+//     Invalid: 2customer, total-Amount, discount Rate
+
+
+// 02. variable names can"t start weith a number , contain hypens , include speces.
+
+
+// 03.
+
+int account_balence = 5000 ;
+int _2ndTransaction = 200 ;
+int transaction_Amount = 150;
+int age = 10 ;
+
+printf(" account balence: %d\n 2ndTransction: %d\n transaction: %d\n age:%d\n", account_balence,_2ndTransaction,transaction_Amount,age);
+return 0;
+
+}
