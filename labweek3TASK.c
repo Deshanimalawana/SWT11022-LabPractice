fce62ca4b0c27dba9decd5a862aa03e2422acd86
xpunsara <punsarawickramasingha4@gmail.com>
2025-03-06 09:30:47 +0530

    Labweek3 TASK 1

---
+++ labweek3TASK.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main () {
+    int studentID;
+    float GPA;
+    char grade;
+    printf("Enter Your studentID:");
+    scanf("%d",&studentID);
+    printf("Enter Your GPA :");
+    scanf("%f",&GPA);
+    printf("Enter Your Grade :");
+    scanf("%s",&grade);
+    return 0;
+}
