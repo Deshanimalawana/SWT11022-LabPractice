23358d5c1462344353b7dc5aa967e3a47d56de71
Mhd-Iz <imthiyasizzni@gmail.com>
2025-03-06 10:42:35 +0530

    Add files via upload

---
+++ labsheet03_Task03.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+
+   /* int AccountBalance =5000;
+    int account_balance =4500;
+    int 2ndtransaction = 200; //2ndtransaction we can't use 2 number in variable
+    int transaction Amount =150; // we can't use space in variable
+    int float =100 // we can't use float variable name it is data type */
+
+    int AccountBalance =5000;
+    int account_balance =4500;
+    int secondndtransaction = 200;
+    int transaction_Amount =150;
+    int number =100;
+
+    printf("AccountBalance is:%d\n",AccountBalance);
+    printf("account_balance is:%d\n",account_balance);
+    printf("secondndtransaction is:%d\n",secondndtransaction);
+    printf("transaction_Amount is:%d\n",transaction_Amount);
+    printf("number is:%d\n",number);
+
+    return 0;
+}
+
