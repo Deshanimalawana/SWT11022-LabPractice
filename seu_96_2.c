1de17b68b90121ac567c3fb73a7fda299c844c36
1NazmiAhamed <ahamednaazmi@gmail.com>
2025-03-06 12:14:30 +0530

    Labsheet 3&4

---
+++ seu_96_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define SPEED_OF_LIGHT 299792458
+#define GRAVITY 9.81
+#define PLANK_CONSTANT 6.626e34
+
+int main() {
+const float BOLTZMANN_ZERO= 1.38E23;
+const int ABSOLUTE_ZERO= -273;
+
+printf("Speed of light: %d m/s\n\n", SPEED_OF_LIGHT);
+printf("Gravity: %.2f\n\n", GRAVITY);
+printf("Planck_constant: %f\n\n",PLANK_CONSTANT);
+printf("Boltzmann zero: %f\n\n",BOLTZMANN_ZERO);
+printf("Absolute zero: %d\n\n",ABSOLUTE_ZERO);
+
+return 0;
+
+}
---
+++ seu_96_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+
+int AccountBalance =5000;
+int account_balance =4500;
+int secondTransection = 200;
+int transection_Amount= 150;
+int floatamount = 100;
+
+printf("AccountBalance: %d\n",AccountBalance);
+printf("account_balance: %d\n",account_balance);
+printf("secondTransection: %d\n",secondTransection);
+printf("transection_Amount: %d\n",transection_Amount);
+printf("floatamount: %d\n",floatamount);
+
+return 0;
+}
