dce31cb2a5fdf6a0b6c6922ee415407df5bc0f59
vihagi-2003 <pulsarabalasoriya@gmail.com>
2025-03-06 10:12:42 +0530

    Add files via upload

---
+++ 02.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+
+int id;
+float GPA;
+char grade;
+
+ id=12345;
+ GPA=3.75;
+ grade='A';
+
+ printf("Student Id: %d\n",id);
+ printf("GPA of the student:%f\n",GPA);
+ printf("Grade of the student:%c\n",grade);
+
+
+id=54321;
+GPA=3.90;
+grade='A+';
+
+
+printf("Updated id:%d\n",id);
+printf("Updated GPA:%f\n",GPA);
+printf("Updated grade:%c\n",grade);
+
+
+return 0;
+}
+
+
