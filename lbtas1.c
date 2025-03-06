4a13b65e76f5e88a018be81359b126a673ef9ff3
eshan <eshansaumaya@gmail.com>
2025-03-06 09:27:47 +0530

    labweek 3 task1

---
+++ lbtas1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+ int studentID;
+ float GPA;
+ char grade;
+ char val;
+
+ studentID = 54321;
+ GPA = 3.90;
+ grade = 'A';
+ val = '+' ;
+
+ printf("studentID %d\n",studentID);
+ printf("GPA %.2f\n",GPA);
+ printf("grade %c%c\n",grade,val);
+return 0;
+
+ }
