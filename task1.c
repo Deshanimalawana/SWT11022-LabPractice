5b3f9428c0569516e5836ca4c3c65f9306633cb5
Ironi-2003 <ironichathurika@gmail.com>
2025-03-06 10:18:11 +0530

    Add files via upload

--- task1.c
+++ task1.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-
 int main() {
 
-   int id=101;
-   char section='B';
-   float marks=89.5;
-   printf("ID:%d, section: %c,marks:%2f\n",id,section,marks);
+    int studentID;
+    float GPA;
+    char grade;
 
-   return 0;
+    studentID=12345;
+    GPA=3.75;
+    grade='A';
 
+   /*printf("studentID:%d\n",studentID);
+    printf("GPA:%.2f\n",GPA);
+    printf("grade:%c",grade);*/
+
+    studentID=54321;
+    GPA=3.90;
+    grade='A+';
+
+    printf("updated studentID: %d\n",studentID);
+    printf("updated GPA:%.2f\n",GPA);
+    printf("updated grade:%c",grade);
+    return 0;
 }
