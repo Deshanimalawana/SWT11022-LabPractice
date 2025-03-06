88f09f80435e6d624c1da080474b76bfa83a0cc0
Anshaf6 <mohammedanshaf2971@gmail.com>
2025-03-06 10:15:53 +0530

    Add files via upload

---
+++ lap03ex3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int AccountBalance = 5000;
+    int account_balance = 4500;
+    int secondTranceaction = 200;
+    int transaction_Amount = 150;
+    int floatamount  = 100;
+
+        printf(" AccountBalance:%d\n",AccountBalance);
+        printf("account balance:%d\n",account_balance);
+        printf("secondTranceaction:%d\n",secondTranceaction);
+        printf("transaction Amount:%d\n",transaction_Amount);
+        printf("floatamount:%d\n", floatamount);
+
+    return 0;
+
+
+
+
+}
