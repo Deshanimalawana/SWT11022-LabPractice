31fc51a5df5a6c80817538e4a5ae2cd9bbab9c54
chinthana-chamika <chinthanachamikarajapaksha@gmail.com>
2025-03-06 10:14:35 +0530

    Add files via upload

---
+++ 015_c.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+    {
+        int studentID;
+        float GPA;
+        char grade;
+        printf(",Enter student ID");
+        scanf("%d",&studentID);
+
+        printf("Grade\n");
+        scanf("%c",&grade);
+
+        printf("GPA");
+        scanf("%f",&GPA);
+
+        printf("%d\n","%c\n","%f\n");
+        return 0;
+    }
