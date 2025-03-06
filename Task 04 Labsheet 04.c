d8f03405da658f8d3897b67adfdd2bc7bef0d96a
Kularasan Jelaxsi <rajanjela22@gmail.com>
2025-03-06 11:10:35 +0530

    Add files via upload

---
+++ Task 04 Labsheet 04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int count = 5;
+    count++; //Increment
+    int total = 10;
+    total += 3; //Add 3 to total
+
+    printf("updated count: %d\n",count);
+    printf("updated Total: %d\n",total);
+
+    return 0;
+}
