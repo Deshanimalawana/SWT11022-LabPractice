de58927b71bbf99cf147c18b964696fffd3194dd
Shahitthiyan <shaketthiyanmurali@gmail.com>
2025-03-06 11:44:31 +0530

    add file

--- Labsheet03_task02.c
+++ Labsheet03_task02.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define SPEED_OF_LIGHT 299792458
-#define GRAVITY 9.81
-#define PLANCK_CONSTANT 6.626e-34
-int main(){
+#define SPEED_OF_LIGHT 299792458//(Speed of light in m/s)
+#define GRAVITY 9.81 //(Acceleration due to gravity in m/s²)
+#define PLANCK_CONSTANT 6.626e-34 //(Planck's constant in J·s)
 
-const float BOLTZMANN_CONSTANT = 1.38E-23;
-const int ABSOLUTE_ZERO = -273;
+int main()
+{
+    const float BOLTZMANN_CONSTANT = 1.38e-23;
+    const int ABSOLUTE_ZERO = -273; //(Absolute zero in Celsius)
 
-printf("The speed of light is :%d\n",SPEED_OF_LIGHT);
-printf("The value of Gravity is :%.2f \n",GRAVITY);
-printf("The value of  is PLANCK_CONSTANT:%.3e\n",PLANCK_CONSTANT);
-printf("The value of  is BOLTZMANN_CONSTANT:%.2e\n",BOLTZMANN_CONSTANT);
-printf("The value of ABSOLUTE ZERO is :%d\n",ABSOLUTE_ZERO);
-
-
-return 0;
-
+    printf("speed of light is:%d\n", SPEED_OF_LIGHT);
+    printf("Gravity is:%.2f\n",GRAVITY);
+    printf("Planck constant is:%.3e\n", PLANCK_CONSTANT);
+    printf("Boltzmann's Constant: %.2e\n", BOLTZMANN_CONSTANT);
+    printf("Absolute Zero: %d\n", ABSOLUTE_ZERO);
 
 
+    return 0;
 }
-
