802250e0b64e0f98a4bf0e483151358d9e72a9bc
eshan <eshansaumaya@gmail.com>
2025-03-06 09:52:30 +0530

    labweek03 task1

---
+++ lbtask1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+ int studentID;
+ float GPA;
+ char grade [] = "A+" ;
+
+ studentID = 54321;
+ GPA = 3.90;
+
+ printf("studentID %d\n",studentID);
+ printf("GPA %.2f\n",GPA);
+ printf("grade %s\n",grade);
+return 0;
+
+ }
