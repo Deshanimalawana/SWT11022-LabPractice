858866753c6e8460b88878ac5d816422503b8853
Kularasan Jelaxsi <rajanjela22@gmail.com>
2025-03-06 11:22:33 +0530

    Add files via upload

---
+++ Task 05 Labsheet 04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+   int value = 6;
+   char result = (value %2== 0) ? 'E':'0';
+   printf("Value is %c (E: Even , 0: odd)\n",result);
+
+   return 0;
+}
