65ee2cb8242dab6479814569698f4cd8a4bdbdca
Mhd-Iz <imthiyasizzni@gmail.com>
2025-03-06 11:42:05 +0530

    Add files via upload

---
+++ labsheet04_task02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+
+    int x =10,y= 4;
+    int isGreaterThen =(x>y);
+    int isEqual = (x==y);
+    int isNotEqual = (x!=y);
+    ;
+
+    printf("Is X grater then Y? %s\n",isGreaterThen ?"yes":"no");
+    printf("Is X equal to Y?%s\n",isEqual ?"yes":"no");
+    printf("Is X not equal to Y?%s\n",isNotEqual ?"yes":"no");
+
+
+    return 0;
+    }
+
+
---
+++ labsheet04_task07.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = 12;
+    int y =6;
+
+     int andResult = x&y:
+     int orResult = x |y;
+     int xorResult = x ^ y:
+
