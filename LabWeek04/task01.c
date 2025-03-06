e42d776acd8173ee1e00a5a55f0ef261aa54e064
piragenthuni <piragenthuni@gmail.com>
2025-03-06 20:46:39 +0530

    task01

---
+++ LabWeek04/task01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10000;
+    int b = 430;
+    int c = 3000000;
+    int d = 1;
+    d = ((a>b) ? ((a>c) ? a : c):((b>c) ? b : c));
+    printf("%d",d);
+    return 0;
+}
+
+
