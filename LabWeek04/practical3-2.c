9812fa27b0934c420496beb799e6dbeab2bcae09
piragenthuni <piragenthuni@gmail.com>
2025-03-06 11:56:39 +0530

    practical 3

---
+++ LabWeek04/practical3-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+int x = 12;
+int y = 6;
+
+int andResult = x & y;
+int orResult = x | y;
+int xorResult = x ^ y;
+int notResult = ~x;
+int leftshift = x << 2;
+int rightshift = y >> 1;
+
+printf("AND Result: %d\n", andResult);
+printf("OR Result: %d\n", orResult);
+printf("XOR Result: %d\n", xorResult);
+printf("NOT Result: %d\n", notResult);
+printf("Left shift Result:%d\n", leftshift);
+printf("Right shift Result: %d\n", rightshift);
+return 0;
+}
