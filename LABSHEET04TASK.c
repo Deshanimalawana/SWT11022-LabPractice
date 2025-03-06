39456a3a57565033ef84d59fb6bb0f6c47da2e3a
Kasun Weerasinghe <kasunsampath057@gmail.com>
2025-03-06 12:19:17 +0530

    Add files via upload

---
+++ LABSHEET04TASK.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+
+
+int a = 10;
+int b = 15;
+int c = 20;
+
+int largest = (a > b) ? ((a > c)? a:c): ((b > c)? b: c);
+printf("largest number:%d\n",largest);
+
+float avarage =(a+b+c)/3.0;
+printf("avarage:%2f\n",avarage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+
+}
