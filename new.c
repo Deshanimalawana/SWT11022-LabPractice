86ab004b15d12f293f733fce238d68c971119e95
dinuka1202 <166311813+dinuka1202@users.noreply.github.com>
2025-03-06 09:56:32 +0530

    Lab weel 03

--- new.c
+++ new.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
-int main() {
-          printf("Hello program\n")
-		 
-          return 20;
+int main(){
+int studentID;
+float GPA;
+char grade[] = "A+";
+
+
+studentID = 54321;
+GPA = 3.90;
+
+
+
+
+printf("Student ID: %d\n",studentID);
+printf("GPA : %.2f\ngrade",GPA);
+printf("Grade : %s\n",grade);
+
+
+
+return 0;
+
 }
