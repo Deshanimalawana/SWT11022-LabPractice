f517b7c35adc67e369c8af71b37e05dfddc8b2cb
shabrafathi <shabrasanoor@gmail.com>
2025-03-07 16:40:49 +0530

    Update task03.c

--- task03.c
+++ task03.c
@@ -8,9 +8,9 @@
 5. int float = 100; = invalid
 
 2)
-3. variable name can not start using numbers.
-4. there is a space between that two words,variable can not get when there is space.
-5. we can not use key word as a variable name.
+3. variable names cannot start with a numbers.
+4. variable names cannot contain spaces.
+5. float is a  reservedkey word in C.
 
 
 3)
@@ -20,13 +20,13 @@ int main(){
 
     int AccountBalance = 5000;
     int account_balance = 4500;
-    int Transaction = 200;
+    int SecondTransaction = 200;
     int transaction_Amount = 150;
     int balance = 100;
 
     printf("%d\n",AccountBalance);
     printf("%d\n",account_balance);
-    printf("%d\n",Transaction);
+    printf("%d\n",SecondTransaction);
     printf("%d\n",transaction_Amount);
     printf("%d\n",balance);
     return 0;
