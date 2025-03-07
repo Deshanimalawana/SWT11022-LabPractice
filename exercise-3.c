70f69079e7d1a81b0c265ea2b902f052bed38a78
haff123 <habsa8013@gmail.com>
2025-03-07 21:07:42 +0530

    Add files via upload

---
+++ exercise-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+   int condition1 = 1; //true
+   int condition2 = 0; //false
+   int condition3 = 2; //true
+
+   int result1= condition1&& condition2;
+   int result2= condition2|| condition3;
+   int result3= !condition2;
+
+   printf("result 1: %s\n", result1? "true" : "false");
+   printf("result 2: %s\n",result2? "true": "false");
+   printf("result 3: %s\n",result3? "true":"false");
+   return 0;
+}
