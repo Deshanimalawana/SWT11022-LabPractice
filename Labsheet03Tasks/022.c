d3f5a848b7e4696e0a8acb7de7f3f1e58be0c2c7
Kavishka31 <kavishkahapuarachchi30@gmail.com>
2025-03-07 23:36:20 +0530

    add lab 03 tasks

---
+++ Labsheet03Tasks/022.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main(){
+    int studentID;
+    float GPA;
+    char* grade;
+
+    studentID = 12345;
+    GPA = 3.75;
+    grade = "A";
+
+    printf("StudentID:%d\n",studentID);
+    printf("GPA:%f\n",GPA);
+    printf("Grade:%s\n",grade);
+
+    studentID = 54321;
+    GPA = 3.90;
+    grade = "A+";
+
+    printf("Update Student ID:%d\n",studentID);
+    printf("Update GPA:%f\n",GPA);
+    printf("Update Grade:%s\n",grade);
+
+
+
+
+
+
+
+    return 0;
+
+
+
+
+
+
+}
