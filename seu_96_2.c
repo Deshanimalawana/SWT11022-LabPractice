fa8a5522e94b8d5178e5fdf6573111d0c79f1fcd
1NazmiAhamed <ahamednaazmi@gmail.com>
2025-03-07 15:52:32 +0530

    Update seu_96_2.c

--- seu_96_2.c
+++ seu_96_2.c
@@ -2,17 +2,17 @@
 
 #define SPEED_OF_LIGHT 299792458
 #define GRAVITY 9.81
-#define PLANK_CONSTANT 6.626e34
+#define PLANCK_CONSTANT 6.626e34
 
 int main() {
-const float BOLTZMANN_ZERO= 1.38E23;
+const float BOLTZMANN_CONSTANT= 1.38e23;
 const int ABSOLUTE_ZERO= -273;
 
-printf("Speed of light: %d m/s\n\n", SPEED_OF_LIGHT);
-printf("Gravity: %.2f\n\n", GRAVITY);
-printf("Planck_constant: %f\n\n",PLANK_CONSTANT);
-printf("Boltzmann zero: %f\n\n",BOLTZMANN_ZERO);
-printf("Absolute zero: %d\n\n",ABSOLUTE_ZERO);
+printf("Speed of light: %d m/s\n", SPEED_OF_LIGHT);
+printf("Gravity: %.2f ms-2\n", GRAVITY);
+printf("Planck_constant: %f\n",PLANCK_CONSTANT);
+printf("Boltzmann zero: %f\n",BOLTZMANN_CONSTANT);
+printf("Absolute zero: %d °C\n",ABSOLUTE_ZERO);
 
 return 0;
 
