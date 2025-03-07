ca6ec598ff5bb6bd64e3254879b8457495f783de
Shahnas-razick <shahnasrazick@gmail.com>
2025-03-07 23:26:20 +0530

    Add files via upload

---
+++ Lab3 Task3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main (){
+   int AccountBalance = 5000;
+   int account_balance = 4500;
+   int SecondTransaction = 200;
+   int transactionAmount = 150;
+   int thirdTransaction =100;
+
+   printf("Account balance is: %d\n",AccountBalance);
+   printf("Account_balance is :%d\n ",account_balance);
+   printf("Second Transaction is : %d\n",SecondTransaction);
+   printf("transaction Amount is : %d\n",transactionAmount);
+   printf("Third Transaction is : %d\n",thirdTransaction);
+
+    return 0;
+
+
+
+}
