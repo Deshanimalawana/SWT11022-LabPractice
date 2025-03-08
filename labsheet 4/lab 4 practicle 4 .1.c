1ef97b057c3a8c40ea8dfe002bdaa804e5f5311f
Thariduj <tharidujayawrdana@gmail.com>
2025-03-08 14:22:16 +0530

    Lab4 tasks added

---
+++ labsheet 4/lab 4 practicle 4 .1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int x  = 10, y = 20 ;
+    printf("----PRE INCREMENT EXAMPLE----\n");
+    printf("Value of x:%d\n",x);
+    printf("Value of x:%d\n",++x);
+    printf("Value of x Incremented:%d\n",x);
+
+    printf("----Post INCREMENT EXAMPLE----\n");
+    printf("Value of y:%d\n",y);
+    printf("Value of y:%d\n",++y);
+    printf("Value of y Incremented:%d\n",y);
+    return 0;
+}
