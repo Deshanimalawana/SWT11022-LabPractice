94b65f44be4826292821fc17f9f7adcd03549f86
Shahitthiyan Murali <shaketthiyanmurali@gmail.com>
2025-03-08 12:00:13 +0530

    Add files via upload

---
+++ Lab03Task03.c
@@ -0,0 +1,18 @@
+#include<Stdio.h>
+
+int main()
+{
+    int AccountBalance = 5000;
+    int account_balance = 4500;
+    int secondTransaction = 200;
+    int transaction_Amount = 150;
+    int transactionfloat = 100;
+
+    printf("Your Account balance is :%d\n",AccountBalance);
+    printf("Your Account_balance is :%d\n",account_balance);
+    printf("Your SecondTransaction is :%d\n",secondTransaction);
+    printf("Your Transaction_Amount is :%d\n",transaction_Amount);
+    printf("Your Transactionfloat is :%d\n",transactionfloat);
+    return 0;
+
+}
