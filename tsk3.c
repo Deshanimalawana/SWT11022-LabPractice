9dcc4eacd7ba4ba10468a9fd0f7332e6a49fa3bc
eshan <eshansaumaya@gmail.com>
2025-03-08 14:34:27 +0530

    labsheet 3 task 3 / 035

---
+++ tsk3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int AccountBalance = 5000;
+    int account_balance = 4500;
+    int secondtransaction = 200;
+    int transaction_Ammount = 150;
+    int float_value = 100 ;
+
+     printf("Account Balace : %d\n",AccountBalance);
+     printf("account balance %d\n",account_balance);
+     printf("Second Transaction: %d\n",secondtransaction);
+     printf("Trasaction Ammount: %d\n", transaction_Ammount);
+     printf("Folat value : %d\n",float_value);
+return 0; 
+}
