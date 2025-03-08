5e3fa4c659b7578960e9e3625fd9587f6efc195c
Ironi-2003 <ironichathurika@gmail.com>
2025-03-08 17:40:04 +0530

    Add files via upload

---
+++ lab sheet 3 task1.c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+
+    int studentID;
+    float GPA;
+    char grade;
+
+    studentID=12345;
+    GPA=3.75;
+    grade='A';
+
+    /*printf("studentID:%d\n",studentID);
+    printf("GPA:%.2f\n",GPA);
+    printf("grade:%c",grade);*/
+
+    studentID=54321;
+    GPA=3.90;
+    grade='A+';
+
+    printf("updated studentID: %d\n",studentID);
+    printf("updated GPA:%.2f\n",GPA);
+    printf("updated grade:%c",grade);
+    return 0;
+}
