96bad50d7ce8137be491ae283789d043fb058bc6
Ishara-ui <isharapramodi3@gmail.com>
2025-03-08 16:27:08 +0000

    Cleaned branch

--- Arithmetic Operations.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-main ()
-{
-    float price = 6.89;
-    int quantity = 5;
-    float total = price * quantity;
-    printf("Total price: %.2f\n", total);
-    return 0;
-}
--- task 6.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("The reverse of %c%c%c is %c%c%c\n", 'X', 'M', 'L', 'L', 'M', 'X');
-    return 0;
-}
