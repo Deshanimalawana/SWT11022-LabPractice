b566e0f02adcffa6274b0b86614ed0d7ce035684
Kavishka31 <kavishkahapuarachchi30@gmail.com>
2025-03-08 14:16:15 +0530

    labsheet 4 task files added

---
+++ labsheet 4/lab 4 practicle 2.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int count = 5;
+    count++; // Increment
+    int total = 10;
+    total += 3; //add 3 to total
+
+    printf("updated count: %d\n",count);
+    printf("updated Total: %d\n",total);
+
+    return 0;
+
+
+}
