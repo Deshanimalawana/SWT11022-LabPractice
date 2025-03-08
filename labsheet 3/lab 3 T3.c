07c38ae864d87f87b86b69c2b05b92e526ca5288
Kavishka31 <kavishkahapuarachchi30@gmail.com>
2025-03-08 14:15:52 +0530

    labsheet 3 task files added

---
+++ labsheet 3/lab 3 T3.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(){
+    int AccountBalance = 5000;
+    int account_balance = 4500;
+    int SecondTransaction = 200; //Invalid - Number in the variable name
+    int transactionAmount = 150; //Invalid - Space in the variable name
+    int floatA = 100; //Invalid - float is not a variable
+
+    printf("Account Balance: %d\n", AccountBalance);
+    printf("account balance: %d\n", account_balance);
+    printf("Second Transaction: %d\n", SecondTransaction);
+    printf("Value: %d\n", floatA);
+
+    return 0;
+
+}
