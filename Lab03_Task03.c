56617199651988ccce1cd6f49d7a488469434a4d
yuvarajah-divejikan <rajanjela22@gmail.com>
2025-03-09 20:47:08 +0530

    Labsheet 03 & 04 files uploaded

---
+++ Lab03_Task03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+    int AccountBalance = 5000;
+    int account_balance = 4500;
+    int SecondTransaction = 200;
+    int transactionAmount = 150;
+    int float_value = 100;
+
+    printf("AccountBalance is :- %d\n", AccountBalance);
+    printf("account_balance is :- %d\n", account_balance);
+    printf("SecondTransaction is :- %d\n", SecondTransaction);
+    printf("transactionAmount is :- %d\n", transactionAmount);
+    printf("float_value is :- %d\n",float_value);
+
+    return 0;
+}
