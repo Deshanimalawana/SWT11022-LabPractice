e46bcebbd0a8c9c6851f3d159b834808463ad982
Ironi-2003 <ironichathurika@gmail.com>
2025-03-09 19:29:15 +0530

    Add files via upload

---
+++ lab sheet 3 task 3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int AccountBalance = 5000;
+    int account_balance = 4500;
+    int transactionAmount = 150;
+    int secondtransaction = 200;
+    int floatvalue = 100;
+
+//    Displaying the values
+    printf("AccountBalance: %d\n", AccountBalance);
+    printf("account_balance: %d\n", account_balance);
+    printf("transactionAmount: %d\n", transactionAmount);
+    printf("secondtransaction: %d\n", secondtransaction);
+    printf("floatvalue: %d\n", floatvalue);
+
+    return 0;
+}
