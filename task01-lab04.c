5cbdc855199a93eb9480042ed7033bf36b02fb0b
shabrafathi <shabrasanoor@gmail.com>
2025-03-09 18:02:39 +0530

    Update task01-lab04.c

--- task01-lab04.c
+++ task01-lab04.c
@@ -1,27 +1,25 @@
-#include<stdio.h>
-int main(){
-    int a = 20;
-    int b = 40;
-    int c = 60;
+#include <stdio.h>
 
-    int largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
-    printf("The largest number is: %d\n", largest);
-    float average = (a + b + c) / 3.0;
+int main() {
+    int a = 10, b = 25, c = 15;
+    int largest;
+    if (a >= b && a >= c) {
+        largest = a;
+    } else if (b >= a && b >= c) {
+        largest = b;
+    } else {
+        largest = c;
+    }
+    
+   int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    printf("The largest number is: %d\n", max);
+    
+   float average = (a + b + c) / 3.0; // Using 3.0 to ensure floating point division
     printf("The average of the three numbers is: %.2f\n", average);
-
+    
     return 0;
-
-
-
-
-
-
-
-
-
-
-
 }
+
 
 
 
