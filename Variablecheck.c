b85d75711793183f63d8e1574e5d38cca21b1503
Hansa Sathsara <hansathsara@gmail.com>
2025-03-10 12:34:55 -0700

    Add files via upload
    Uploading Operator from c programming

---
+++ Variablecheck.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  int AccountBalance=5000;
+  int account_balance=4500;
+
+  printf("AccountBalance: %d",AccountBalance);
+  printf("account_balance: %d",account_balance);
+
+
+  return 0;
+
+}
