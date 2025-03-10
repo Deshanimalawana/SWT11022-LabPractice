95c76a018ee3a68e20879856bffad48cd0058713
samila2003 <samiladulanjani5@gmail.com>
2025-03-10 21:41:32 +0530

    Add files via upload

---
+++ lab4task3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int x=5,y=4;
+
+    int and_result = x&y;
+    int or_result = x|y;
+    int xor_result = x^y;
+
+
+    printf("Bitwise AND (x&y)= %d\n",and_result);
+    printf("Bitwise OR (x|y)= %d\n",or_result);
+    printf("Bitwise XOR (x^y)= %d\n", xor_result);
+    return 0;
+
+
+
+
+
+
+}
