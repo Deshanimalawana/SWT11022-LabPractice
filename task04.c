fd8e92e1ac469bae181cf114ae6feddcc828e948
nuha-mhmf <mhmfnuha@gmail.com>
2025-03-10 20:45:36 +0530

    Add files via upload

---
+++ task04.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a= 5, b=10, c=15;
+
+    int largest = (a>b && a>c) ? a : (b>a && b>c)? b:c;
+    printf("the largest number is %d\n", largest);
+
+    float average = (a+b+c)/3.0;
+    printf ("The average of the three numbers is : %.2f\n", average);
+
+
+    return 0;
+
+
+}
