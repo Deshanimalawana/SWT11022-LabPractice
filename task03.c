a819ec0ffbf7fd8d1565a61c3a3c8bfe44212141
nuha-mhmf <mhmfnuha@gmail.com>
2025-03-10 20:45:17 +0530

    Add files via upload

---
+++ task03.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+
+int AccountBalance= 5000;
+int account_balance= 4500;
+int transactionAmount= 150;
+int price= 100;
+
+
+printf("AccountBalance is %d\n", AccountBalance);
+printf("account_balance is %d\n", account_balance);
+printf("transactionAmount is %d\n", transactionAmount);
+printf("price is %d", price);
+
+return 0;
+}
