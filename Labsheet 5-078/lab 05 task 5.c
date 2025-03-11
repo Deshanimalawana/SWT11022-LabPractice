b93687ee48fa30d1daf0cbe10faeae28a4f717d2
WMAPSABEYRATHNE <Akashabeyrathne0519@gmail.com>
2025-03-11 10:24:28 +0530

    Add files via upload

---
+++ Labsheet 5-078/lab 05 task 5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+ int marks=85;
+ if (marks>=90)
+    printf("The grade is Excellent");
+ else if(marks>=75)
+    printf("The grade is very Good");
+ else if(marks>=50)
+    printf("The grade is Good");
+ else
+    printf("The grade is fail");
+}
