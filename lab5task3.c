bac4a7f72f9ca6807df751fbbc70c81790e6bd66
samila2003 <samiladulanjani5@gmail.com>
2025-03-11 10:17:52 +0530

    Add files via upload

---
+++ lab5task3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int number =-7;
+    if(number>0){
+        printf ("The number is a positive number");
+    }else if(number<0){
+        printf("The number is a negative number");
+    }else{
+        printf("the number is zero");
+    }
+return 0;
+}
