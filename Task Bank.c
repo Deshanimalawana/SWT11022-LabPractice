27bbd0821ba0f91e67c6591409d95e680af6b30b
xpunsara <punsarawickramasingha4@gmail.com>
2025-03-11 22:58:03 +0530

    labsheet 3 & 4 /013

---
+++ Task Bank.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int AccountBalance = 5000;
+    int account_balance = 4500;
+    int secondTransaction = 200;
+    int transactionAmount = 150;
+    int floatAmount = 100;
+
+    // Printing the values
+    printf("Account Balance 1: %d\n", AccountBalance);
+    printf("Account Balance 2: %d\n", account_balance);
+    printf("Second Transaction: %d\n", secondTransaction);
+    printf("Transaction Amount: %d\n", transactionAmount);
+    printf("Float Amount: %d\n", floatAmount);
+
+    return 0;
+}
