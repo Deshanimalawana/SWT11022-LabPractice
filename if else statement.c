0793bcaefbd3ab11f4fb6472491a74c2b2c17869
Shahnas-razick <shahnasrazick@gmail.com>
2025-03-11 10:23:45 +0530

    Add files via upload

---
+++ if else statement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+   int marks=42;
+   if (marks >= 50)
+   {
+       printf("You passed");
+   }
+   else
+   {
+       printf("you failed");
+   }
+  return 0;
+
+
+
+}
