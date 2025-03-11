029bf2404be11542dbc8e7803fc61abd89c7fa92
Rukshana0924 <rukshlath2002@gmail.com>
2025-03-11 10:42:15 +0530

    Add files via upload

---
+++ lab5 tssk 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a;
+printf("Enter a Number :");
+scanf("%d",&a);
+if (a%2==0){
+    printf("The number is Even ");
+}else{
+printf("The number is odd");
+}
+return 0;
+}
