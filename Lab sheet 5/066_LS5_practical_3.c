d4bf80eaeb34bddf1292e4014d3642338c9d987b
Apinesh <apineshtharmarasa105@gmail.com>
2025-03-11 10:27:46 +0530

    Add files via upload

---
+++ Lab sheet 5/066_LS5_practical_3.c
@@ -0,0 +1,14 @@
+#include <Stdio.h>
+int main() {
+  int number= -676;
+  if(number>0) {
+    printf("This number is positive\n");
+  }
+   else if(number<0) {
+    printf("This number is negative\n");
+   }
+    else {
+        printf("This number is zero\n");
+    }
+
+return 0;
