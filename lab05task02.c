3e81f3d72b1db021091c8d1b4a3a492be12c866c
iniyavansk <iniyavanski2003@gmail.com>
2025-03-11 10:33:31 +0530

    labseet05

---
+++ lab05task02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int marks;
+printf("Enter the marks: ");
+scanf("%d",&marks);
+if (marks>=90)
+{printf("Excellent");}
+else if (marks >=75)
+{printf("Very good");}
+else if (marks >=50)
+{printf("good");}
+else
+{printf("fail");}                                ;
+}
