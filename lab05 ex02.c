d028ff62289e131cb56c90cc307a5e586cd36c65
Kavindu061 <kavindujanith83@gmail.com>
2025-03-11 10:30:34 +0530

    Add files via upload

---
+++ lab05 ex02.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int marks=42;
+
+    if(marks >= 50){
+        printf("you passed the exam.\n");
+    }
+    else{
+        printf("you failed the exam.\n");
+    }
+    return 0;
+}
