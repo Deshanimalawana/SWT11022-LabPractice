b5a0fd459f47e4141fbd88f2d98b6fc59cb16631
upekshi <upekshilavanya13@gmail.com>
2025-03-11 22:37:35 +0530

    Add files via upload

---
+++ 5task_4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+  int main(){
+  int a;
+  int b;
+  int c;
+
+    printf("Enter the number:");
+    scanf ("%d %d %d", &a ,&b ,&c);
+
+    if ( a>b && a>c) {
+     printf ("The largest number is: %d\n",a);
+    }else if(b>a && b>c){
+    printf ("The largest number is: %d\n",b);
+    }else if (c>a && c>b){
+    printf ("The largest number is: %d\n",c);
+    }
+    return 0 ;
+  }
