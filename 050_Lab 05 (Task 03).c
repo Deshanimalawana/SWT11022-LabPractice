8ad7ad51467268b4db9ecbeb792b0154a5e6c7aa
Anusha825-w <anushamunasinghe825@gmail.com>
2025-03-11 19:34:58 +0530

    Add files via upload

---
+++ 050_Lab 05 (Task 03).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int age;
+
+    printf("Enter age:");
+    scanf("%d",&age);
+
+    if(age<18){
+        printf("Not eligible to vote.");
+    }else{
+        printf("Eligible to vote.");
+    }
+
+    return 0;
+}
