76ea3e9dae79fa6cc13f4656c763f0c1f95ec433
ImeshChathura <88792235+imeshcha@users.noreply.github.com>
2025-03-11 10:26:24 +0530

    Labsheet 05 tasks (032)

---
+++ 032-task01-labsheet05.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int val = 20;
+    if ( val % 2 == 0) {
+        printf ("The number is even");
+    }
+    else {
+        printf ("the number is odd");
+    }
+    return 0;
+}
