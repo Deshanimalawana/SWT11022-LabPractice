0b3874f9f39a4f5a43a7f2e2b3c8b1b1a4e7c18a
Tashmika Jayamanna <tashmikameriyan@gmail.com>
2025-03-11 10:28:50 +0530

    Add files via upload

---
+++ 105_Lab5_Task1.c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main () {
+
+    int number = 7;
+
+    if(number  % 2 == 0){
+    printf("It's an even number\n");
+    }
+    else{
+        printf("It's an odd number\n");
+    }
+    return 0;
+}
