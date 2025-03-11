f94317f7e8bb766037b2b4a9ab6d1911996ad4fe
Kularasan Jelaxsi <rajanjela22@gmail.com>
2025-03-11 10:18:57 +0530

    Add files via upload

---
+++ 068  labsheet 05 IF ELSE IF ELSE STATEMENT.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number = -7;
+    if(number>0){
+        printf("The number is positive.");
+    }else if (number<0){
+    printf("The number is negative.");
+    }else {
+    printf("The number is Zero.");
+    }
+    return 0;
+}
---
+++ 068 Labsheet 05 Task 05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("Enter a Number :");
+    scanf("%d",&num);
+
+    if(num>0){
+        printf("The number is positive\n");
+    }else if(num<0){
+        printf("The number is negative\n");
+    }else{
+        printf("The number is zero\n");
+    }
+    return 0;
+}
