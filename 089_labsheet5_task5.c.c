8d0e5ab6e0c8d19d5e4a59c8c33cb2c124d7966c
HashiLihinikadwa14 <hashilihinikaduwa456@gmail.com>
2025-03-13 21:59:52 +0530

    Add files via upload

---
+++ 089_labsheet5_task5.c.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() {
+
+    int number;
+
+    printf("Enter a number:");
+    scanf("%d", &number);
+
+    if(number >0){
+        printf("The number is positive.");
+    }
+    else if (number <0){
+        printf("The number is negative.");
+    }
+    else{
+        printf("The number is Zero.");
+    }
+    return 0;
+
+    }
