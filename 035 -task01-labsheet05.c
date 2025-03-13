329ff7351463e1bb3423e4154b81e3c8ce44ab87
eshan <eshansaumaya@gmail.com>
2025-03-13 22:13:25 +0530

    Update 035 -task01-labsheet05.c

--- 035 -task01-labsheet05.c
+++ 035 -task01-labsheet05.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
-int main (){
-int num =20;
- if (num % 2 == 0){
-    printf ("The number is even");
- }else
-  printf("The number is odd");
-return 0;
-}
+ #include <stdio.h>
+ int main (){
+ int num ;
+ printf ("Enter a number :");
+ scanf ("%d",& num);
+  if (num % 2 == 0){
+     printf ("The number is even");
+  }else
+   printf("The number is odd");
+ return 0;
+ }
