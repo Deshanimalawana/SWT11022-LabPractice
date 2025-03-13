f18262ec5d8f16514f4b34fde7f231074c8a58d3
ImeshChathura <88792235+imeshcha@users.noreply.github.com>
2025-03-13 23:13:58 +0530

    Update 032-task04-labsheet05.c

--- 032-task04-labsheet05.c
+++ 032-task04-labsheet05.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main () {
     int num1, num2, num3;
-    printf("enter three numbers : ");
+    printf("Enter three numbers : ");
     scanf("%d %d %d", &num1, &num2, &num3);
 
     if (num1 > num2 && num1 > num3) {
