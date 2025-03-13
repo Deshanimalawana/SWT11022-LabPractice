c9bdd16553c1e4cb38eb9a63c2bd905591c36a79
ImeshChathura <88792235+imeshcha@users.noreply.github.com>
2025-03-13 22:51:04 +0530

    Update 032-task03-labsheet05.c

--- 032-task03-labsheet05.c
+++ 032-task03-labsheet05.c
@@ -5,9 +5,9 @@ int main (){
     scanf("%d", &age);
 
     if (age >= 18) {
-        printf("Eligible to vote");
+        printf("Eligible to vote\n");
     } else {
-        printf("Not eligible to vote");
+        printf("Not eligible to vote\n");
     }
     return 0;
 }
