29e8b7906c1bdf2033463c7a4e64349e46fbe2ac
Isuru sampath fernando <isurusampath2322@gmail.com>
2025-03-14 20:10:04 +0530

    Add files via upload

---
+++ LABSHEET03/Task01_5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int studentID = 54321;
+    float GPA = 3.90;
+    char grade = 'A';
+    char val = '+';
+
+    printf("Updated studentID:%d\n",studentID);
+    printf("Updated GPA:%.2f\n",GPA);
+    printf("Updated grade:%c %c\n",grade,val);
+
+    return 0;
+}
---
+++ LABSHEET03/Task03_3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int AccountBalance = 5000;
+    int account_balance = 4500;
+    int Transaction_02 = 200;
+    int transactionAmount = 150;
+    int float_value = 100;
+
+    printf("Account Balance:%d\n",AccountBalance);
+    printf("Account Balance:%d\n",account_balance);
+    printf("2ND Transaction:%d\n",Transaction_02);
+    printf("Transaction Amount:%d\n",transactionAmount);
+    printf("Float value:%d\n",float_value);
+
+    return 0;
+}
