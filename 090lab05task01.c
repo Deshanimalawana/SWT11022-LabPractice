f24c0b59b1e03b812bc479889a65c0b89c2283bb
ANafeesh <ahamednafeesh2003@gmail.com>
2025-03-14 10:41:21 +0530

    Update 090lab05task01.c

--- 090lab05task01.c
+++ 090lab05task01.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main(){
 
-    int number=7;
+    int number;
+    printf("Enter a number: ");
+    scanf("%d",&number);
+    
     if(number%2==1){
         printf("The number is odd.\n");
         }
