ac27c58de08258ff8d89309a59f1f6f985bf84ba
hansathsara@gmail.com <hansathsara@gmaail.com>
2025-03-16 07:03:01 -0700

    Delete unnecessary files

--- Task1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int quantity = 4;
-    float price = 59.999;
-    float total = quantity*price;
-
-    printf("Total: %f", total);
-    return 0;
-}
--- question 6.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-
-    printf("The reverse of XML is LMX");
-
-    return 0;
-
-}
