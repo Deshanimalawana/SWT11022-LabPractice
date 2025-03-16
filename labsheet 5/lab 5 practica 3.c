51ac5fd332c65cb6412a26282607972eab5f2814
Thariduj <tharidujayawrdana@gmail.com>
2025-03-16 22:35:23 +0530

    labsheet 5 task added

---
+++ labsheet 5/lab 5 practica 3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int number = 0 ;
+    if(number>0){
+        printf("THe number is positive.");
+    }
+    else if(number<0){
+        printf("The number is negative.");
+    }
+    else{
+        printf("The number is zero.");
+    }
+    return 0;
+
+}
