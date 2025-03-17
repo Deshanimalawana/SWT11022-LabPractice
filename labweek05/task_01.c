7a23129b7575ab64266110cd4c24b7cf680a55ee
sachindukavishka <sachindukavishka10@gmail.com>
2025-03-17 21:22:26 +0530

    Labsheet 5 files added

---
+++ labweek05/task_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+int number;
+printf("Enter a Number");
+scanf("%d",&number);
+if(number%2==1) {
+    printf("The number is odd");
+}
+else {
+    printf("The number is even");
+}
+}
