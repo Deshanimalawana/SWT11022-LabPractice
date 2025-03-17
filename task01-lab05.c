26b59220e659df53175a382198f412de9ed89406
shabrafathi <shabrasanoor@gmail.com>
2025-03-17 17:03:24 +0530

    Add files via upload

---
+++ task01-lab05.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if (num % 2 == 0){
+        printf("this number is even");
+    }
+
+    else{
+        printf("the number is odd");
+    }
+
+
+    return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
