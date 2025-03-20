fae1c274f00c6295e21ce01ef0c7ad46e6d6a3e7
Isuru sampath fernando <isurusampath2322@gmail.com>
2025-03-20 11:06:38 +0530

    Update task03.c

--- LABSHEET06/task03.c
+++ LABSHEET06/task03.c
@@ -15,6 +15,18 @@ int main(){
     case 3:
         printf("Wedenesday");
         break;
+    case 4:
+        printf("Thursday");
+        break;
+    case 5:
+        printf("Friday");
+        break;
+    case 6:
+        printf("Saturday");
+        break;
+    case 7:
+        printf("Friday");
+        break;
     default:
         printf("Other day\n");
     }
