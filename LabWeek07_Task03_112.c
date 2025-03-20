a044f930cb4861145507fe35e28be3dcadf22315
Mhd-Iz <imthiyasizzni@gmail.com>
2025-03-20 12:21:04 +0530

    Add files via upload

---
+++ LabWeek07_Task03_112.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int number=1;
+    do {
+
+      printf("%d\n",number);
+        number+=1;
+    } while (number<=10);
+
+
+
+    return 0;
+    }
---
+++ LabWeek07_Task04_112.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int number;
+    number =1;
+
+    while (number>0){
+        printf("Enter a number: ");
+        scanf("%d",&number);
+
+    }
+    return 0;
+    }
