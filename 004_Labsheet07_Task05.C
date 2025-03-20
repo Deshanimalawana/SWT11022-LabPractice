b7b04e7f8075f1f2a74c1b18c99d5306b6ac0559
Hasini110 <hasinitheekshana48@gmail.com>
2025-03-20 12:23:32 +0530

    Add files via upload

---
+++ 004_Labsheet07_Task05.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (){
+    for (int i =2; i <=20; i++){
+        if (i%2 ==1){
+                continue;
+        }
+   printf("%d ",i);
+
+    }
+   return 0;
+   }
+
+
+
+
+
+
+
+
