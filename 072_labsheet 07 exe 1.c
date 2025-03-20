1cfc64c9bd158f1508db5179e42bd99b5b5ebcf8
JeyashankarKamsa <jeyashankarkamsa@gmail.com>
2025-03-20 11:05:14 +0530

    Add files via upload

---
+++ 072_labsheet 07 exe 1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+    int main(){
+    char english;
+    for(english='a';english<='z';english++){
+        printf("%c \n",english);
+    }
+    return 0;
+}
---
+++ 072_labsheet 07 exe 4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int number;
+    while(2){
+    printf("Enter Your Number:");
+    scanf("%d",&number);
+    if(number<0){
+        break;
+            }
+        }
+    printf("You entered Negative number.");
+    return 0;
+
+}
