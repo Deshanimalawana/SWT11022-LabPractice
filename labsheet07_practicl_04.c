c884f86852703f2f49e95a22241db78a7259901c
Mohommadhu Afnan <mohommadhuafnan756@gmail.com>
2025-03-20 12:19:45 +0530

    Add files via upload

---
+++ labsheet07_practicl_04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int firstEven =0;
+
+    for (int i =1; ;i++){
+        if(i%2==0){
+            firstEven =i;
+            break;
+        }
+    }
+    printf ("the first even number is :%d\n",firstEven);
+    return 0;
+}
