ee57d5175f6917f440518925a3b27c109eb3a567
Maheli88 <limanthamaheli@gmail.com>
2025-03-20 12:15:45 +0530

    Add files via upload

---
+++ units.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    int units;
+    float bill=0.0;
+    int user=1;
+
+    do {
+        printf("Enter the number of units: ");
+        scanf("%d", &units);
+        if (units<=50)
+        {
+            bill=units*0.50;
+        }else if(units<=150)
+        {
+             bill=50*0.50+(units-50)*0.75;
+        }else if(units<=250)
+        {
+            bill=50*0.50+100*0.75+(units-150)*120;
+        }else
+        {
+            bill=50*0.50+100*0.75+100*1.20+(units-250)*1.50;
+        }
+        printf("The total electricity bill is:Rs%.2f\n", bill);
+        user++;
+    }while (user<=10);
+
+    return 0;
+}
