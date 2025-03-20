8e820a5a7f0a0c56aebb908fc33c5bcfd1249e5d
Kasun Weerasinghe <kasunsampath057@gmail.com>
2025-03-20 11:33:12 +0530

    Add files via upload

---
+++ labsheet07prc3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int input;
+
+    do{
+        printf("enter a positive number:");
+        scanf("%d",&input);
+    }
+    while(input <= 0);
+    printf("you entered a positive number:%d\n",input);
+return 0;
+}
---
+++ labsheet07task01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    char chr;
+
+    for(chr ='a'; chr <= 'z'; chr++){
+    printf("%c ",chr);
+    }
+
+    printf("\n");
+
+return 0;
+}
