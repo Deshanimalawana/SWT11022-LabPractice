5b4f68d5f7ec64ca648f25d00b5d3ac36abc39ba
Ironi-2003 <ironichathurika@gmail.com>
2025-03-20 12:26:56 +0530

    Add files via upload

---
+++ Lab sheet7 task 4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+     int input;
+
+     do {
+        printf("Enter a negative number: ");
+        scanf("%d", &input);
+     }
+     while (input >=0);
+        printf("You entered a negative number: %d\n", input);
+     return 0;
+
+
+
+
+}
