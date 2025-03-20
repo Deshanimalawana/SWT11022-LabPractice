ed9e7884a780fff894c4eacb6c35eb183c6d7bd2
GayaN948 <gayanudayanga1221@gmail.com>
2025-03-20 10:17:00 +0530

    Add files via upload

---
+++ lab 06 task 05.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main() {
+    int dayOfWeek;
+
+    printf("Enter a number (1-7) for the day of the week: ");
+    scanf("%d", &dayOfWeek);
+
+     switch(dayOfWeek) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:
+            printf("Other day\n");
+            break;
+    }
+
+    return 0;
+}
