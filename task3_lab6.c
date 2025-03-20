7326908b2e9ebb230800438c0ead29ffe6f3d97d
samila2003 <samiladulanjani5@gmail.com>
2025-03-20 10:35:51 +0530

    Add files via upload

---
+++ task3_lab6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+ int dayofWeek = 1;
+
+ switch (dayofWeek){
+  case 1:
+      printf("Monday\n");
+      break;
+  case 2:
+      printf("Tuesday\n");
+      break;
+  case 3:
+      printf("Wednesday");
+      break;
+  default:
+    printf("Other day\n");
+
+ }
+return 0;
+}
