62d73e8f19598ee96fe0464a05eb68b4e1acf07b
damsara10 <damsaranawarathnasgn@gmail.com>
2025-03-22 02:05:11 +0530

    Add files via upload

---
+++ LabSheet03/Task01.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int student = 12345;
+    float GPA = 3.75;
+    char grade = 'A';
+
+
+    printf("Student ID:%d,\nGPA : %.2f,\nGrade :%c",student,GPA,grade);
+
+    return 0;
+
+
+}
---
+++ LabSheet03/Task02.01.c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+ int main(){
+     int AccountBlance = 5000;
+     int account_balance = 4500;
+     int Transaction_2nd = 200;
+     int transactionAmount = 150;
+     int Amount = 100;
+
+     printf("AccountBlance:%d\n",AccountBlance);
+     printf("account:%d\n",account_balance);
+     printf("Transaction_2nd:%d\n",Transaction_2nd);
+     printf("transactionAmount:%d\n",transactionAmount);
+     printf("Amount:%d\n",Amount);
+
+     return 0 ;
+ }
