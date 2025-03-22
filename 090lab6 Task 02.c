6806faf7bc109bd6f9038894902e95ba63cf41ec
ANafeesh <ahamednafeesh2003@gmail.com>
2025-03-22 12:29:06 +0530

    Update 090lab6 Task 02.c

--- 090lab6 Task 02.c
+++ 090lab6 Task 02.c
@@ -4,15 +4,14 @@ int main (){
 int age = 60;
 int income = 50000;
 
-if(age>=60){
-    if(income<60000){
-        printf("Eligible for tax deduction\n");
+    if(age>=60){
+        if(income<60000){
+            printf("Eligible for tax deduction\n");
+        }else{
+            printf("Not Eligible for tax deduction\n");
+        }
     }else{
-    printf("Not Eligible for tax deduction\n");
-}
-}else{
-    printf("Not Eligible for tax deduction\n");
-}
+        printf("Not Eligible for tax deduction\n");
+    }
 return 0;
 }
-
