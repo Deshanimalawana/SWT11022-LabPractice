4b3c0f86b5b8be775830cdb73d74575a0a540743
miabdulrahman <m.i.abdulrahmanar@gmail.com>
2025-03-22 14:01:24 +0530

    Update 023_Lab06_task1.c

--- 023_Lab06_task1.c
+++ 023_Lab06_task1.c
@@ -1,42 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main()
 {
-
     float unit;
     printf("Enter your electricity UNIT:");
     scanf("%f",&unit);
-    float cound1=unit*0.5;
+    float first50=unit*0.5;
     float a=unit-50;
-    float b=50*0.5;
-    float c=a*0.75;
-    float d=50*0.5+100*0.75;
-    float e=50*0.5+100*0.75+100*1.20;
-    float _4rth=unit-250, bill4=_4rth*1.5;
+    float first100=a*0.75;
+    float b=unit-150;
+    float second100=b*1.20;
+    float c=unit-250;
+    float above250=c*1.50;
 
-    float _3rd=unit-150, bill3=_3rd*1.20;
+
     if(unit<=50){
 
-        printf("Total bill is: %f",cound1);
+        printf("Total bill is: %f",first50);
          }
 
     else if(unit<=150){
 
-        printf("Total bill is: %f", c+b);
+        printf("Total bill is: %f",first50+first100);
          }
     else if(unit<=250){
 
-        printf("Total bill is: %f", d+bill3);
+        printf("Total bill is: %f",first50+first100+second100);
     }
-   else if(unit<=350){
+   else if(unit>250){
 
-        printf("Total bill is: %f", e+bill4)
-;        }
-
-
-
-
-
+        printf("Total bill is: %f",first50+first100+second100+above250);
+            }
     return 0;
 }
