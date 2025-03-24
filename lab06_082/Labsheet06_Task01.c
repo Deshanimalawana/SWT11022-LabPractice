d3efe41885d2f583a7e40e62b170244c77c286cc
NOBLE <ammuabbas2003@gmail.com>
2025-03-24 20:23:12 +0530

    Labsheet06_Task01.c

--- lab06_082/Labsheet06_Task01.c
+++ lab06_082/Labsheet06_Task01.c
@@ -1,39 +1,23 @@
 #include <stdio.h>
 
-int main( ){
+int main() {
     int units;
     float Rate;
 
-    printf("Enter the units:");
-    scanf("%d",&units);
+    printf("Enter the units: ");
+    scanf("%d", &units);
 
-    if(units<=50){
-            Rate=units*0.50;
-
-    }else if(units>=50){
-            Rate=(units-50)*0.75+(50*0.50);
-
-    }else if(units>=100){
-            Rate=(units-100)*1.20+(50*0.50)+(50*0.75);
-
-    }else if(units>250){
-             Rate=(units-250)*1.50+(50*0.50)+(50*0.75)+(100*1.20);
+    if(units <= 50) {
+        Rate = units * 0.50;
+    } else if(units <= 150) {
+        Rate = (50 * 0.50) + (units - 50) * 0.75;
+    } else if(units <= 250) {
+        Rate = (50 * 0.50) + (100 * 0.75) + (units - 150) * 1.20;
+    } else if (units>=250){
+        Rate = (50 * 0.50) + (100 * 0.75) + (100 * 1.20) + (units - 250) * 1.50;
     }
 
-    printf("Total bill:%.2f\n",Rate);
+    printf("Total bill: %.2f\n", Rate);
     return 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
