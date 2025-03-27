887847f9aa2978f74f7278aaa213f2def18017ed
hafrathusam <nuhafrath2023@gmail.com>
2025-03-27 10:39:59 +0530

    Update Task1_ Write a function to check whether a given number is prime.c

--- Labsheet8/Task1_ Write a function to check whether a given number is prime.c
+++ Labsheet8/Task1_ Write a function to check whether a given number is prime.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 
-int Prime(int num) {
-    if (num < 2) return 0;
+int isPrime(int num) {
+    if (num < 2) {
+        return 0;
+    }
     for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) return 0;
+        if (num % i == 0){
+        return 0;
+    }
     }
     return 1;
 }
@@ -15,7 +19,7 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &number);
 
-    if (Prime(number))
+    if (isPrime(number))
         printf("%d is a prime number.\n", number);
     else
         printf("%d is not a prime number.\n", number);
