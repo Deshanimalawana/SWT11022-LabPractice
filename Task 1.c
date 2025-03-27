6c7b21539135adabd44dcffb40572b0e09cdbd2d
haff123 <habsa8013@gmail.com>
2025-03-27 11:28:05 +0530

    Task 1.c

--- Task 1.c
+++ Task 1.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 int prime(int num) {
-      if (number>0){
-      if(num % num == 0 ){
-            return 1;}
-      else{
+      if (number<=1){
             return 0;}
+      for(int i=2; i*i <=num; i++){
+            if(num%i ==0){
+            return 0;} }
 }
 
 int main(){
     int number;
     printf("enter the number :");
     scanf("%d",&number);
-    prime(number);
 
-          printf("this is prime number");}
+    if(prime(number)){
+         printf("%d  is prime number.\n",number);}
+    else{
+          printf("%d  is not prime number.\n",number);} 
 
 return 0;
 }
