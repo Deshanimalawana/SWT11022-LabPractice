fa59d989dcbb87feb278ec8f990d8974162709ae
hafrathusam <nuhafrath2023@gmail.com>
2025-03-27 10:41:13 +0530

    Update task2_ Write a recursive function to calculate the nth Fibonacci number.c

--- Labsheet8/task2_ Write a recursive function to calculate the nth Fibonacci number.c
+++ Labsheet8/task2_ Write a recursive function to calculate the nth Fibonacci number.c
@@ -2,18 +2,20 @@
 
 
 int fibonacci(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
+    if (n <=1)
+        return 0;
+
+        return 1;
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main() {
-    int n, i;
+    int num;
     printf("Enter the number of terms: ");
-    scanf("%d", &n);
+    scanf("%d", &num);
 
-    printf("Fibonacci sequence up to %d terms:\n", n);
-    for (i = 0; i < n; i++) {
+    printf("Fibonacci sequence up to %d terms:\n", num);
+    for (int i = 0; i < num; i++) {
         printf("%d ", fibonacci(i));
     }
 
