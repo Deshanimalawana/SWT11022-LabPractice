f4c91e9065b1e3d546a726c91cfbf530c1322406
prabash47 <pasinduprabash77@gmail.com>
2025-03-27 11:07:39 +0530

    Add files via upload

---
+++ 049 labsheet 08 task 01.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int isPrime(int num);
+
+int main() {
+    int number;
+
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+
+    if (isPrime(number)) {
+        printf("%d is a prime number.\n", number);
+    } else {
+        printf("%d is not a prime number.\n", number);
+    }
+
+    return 0;
+}
+
+int isPrime(int num) {
+
+    if (num <= 1) {
+        return 0;
+    }
+
+
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
