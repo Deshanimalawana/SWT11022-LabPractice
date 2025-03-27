039afb3b063b26c8386267d77ade63eba644e348
Dhusanthika <thusanthika2003@gmail.com>
2025-03-27 11:35:05 +0530

    Update 074_08_task3.c

--- 074_08_task3.c
+++ 074_08_task3.c
@@ -14,6 +14,6 @@ int main()
     printf("Enter the two numbers");
     scanf("%d %d",&num1,&num2);
 
-    printf("The gcd is: %d,%d",num1,num2,gcd(num1,num2));
+    printf("The gcd is: %d,%d,%d",num1,num2,gcd(num1,num2));
     return 0;
 }
