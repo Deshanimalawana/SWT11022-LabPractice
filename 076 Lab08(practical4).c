e8a77160a0a1741616cd4a026e7612228f3561ff
vihagi-2003 <pulsarabalasoriya@gmail.com>
2025-03-27 10:35:12 +0530

    Add files via upload

---
+++ 076 Lab08(practical4).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int number;
+
+    printf("Hello,World\n");
+
+    printf("Enter a number:");
+    scanf("%d",&number);
+    printf("You entered:%d\n",number);
+
+
+    return 0;
+
+
+}
