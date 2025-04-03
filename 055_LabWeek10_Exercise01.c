1e1a1411a6cafff66d134b5e3a6ec3bdad6d60e7
Rashmi-Juli <rashmijuliyange@gmail.com>
2025-04-03 12:12:05 +0530

    Add files via upload

---
+++ 055_LabWeek10_Exercise01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    int marks[5];
+    marks[0] = 98;
+
+    int ages[5] = {21, 21, 23, 24, 25};
+    ages[1] = 27;
+
+    int students_marks[5];
+
+    printf("Enter marks for 5 students:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Student %d: ", i + 1);
+        scanf("%d", &students_marks[i]);
+    }
+
+    printf("\nStored Marks: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", students_marks[i]);
+    }
+
+    return 0;
+}
