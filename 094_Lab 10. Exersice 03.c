b53cf2e3e1707131080752fe78f3f51343eed409
HirunikaRajapaksha <hirurajapaksha2003@gmail.com>
2025-04-03 12:39:53 +0530

    Add files via upload

---
+++ 094_Lab 10. Exersice 03.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    float numbers[10];
+
+    float max_value;
+
+    printf("Enter 10 floating point numbers:\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("Number %d: ", i + 1);
+        scanf("%f", &numbers[i]);
+    }
+
+    max_value = numbers[0];
+
+    for (int i = 1; i < 10; i++) {
+        if (numbers[i] > max_value) {
+            max_value = numbers[i];
+        }
+    }
+
+    printf("\nThe largest number is: %.2f\n", max_value);
+
+    return 0;
+}
