03489ee8ea1d6b43d5db699bd00afde6ad5efc24
Chalana Dilshan <chalanadilshanrcc@gmail.com>
2025-04-03 12:49:48 +0530

    027_Lab_10

---
+++ 027_Lab_10/Labsheet 10 Exercise 04.c
@@ -0,0 +1,23 @@
+int main() {
+    char letters[10];
+
+    printf("Enter 10 characters:\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("Character %d: ", i + 1);
+        scanf(" %c", &letters[i]);
+    }
+
+    printf("\nCharacter Checking:\n");
+    for (int i = 0; i < 10; i++) {
+        if (letters[i] >= 'A' && letters[i] <= 'Z') {
+            printf("%c is an Uppercase letter\n", letters[i]);
+        } else if (letters[i] >= 'a' && letters[i] <= 'z') {
+            printf("%c is a Lowercase letter\n", letters[i]);
+        } else {
+            printf("%c is not an alphabet\n", letters[i]);
+        }
+    }
+
+    return 0;
+}
