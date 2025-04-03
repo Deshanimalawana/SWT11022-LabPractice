bd8ef24651c39d2c21ec7c0c6d8fd356b5f53842
Mariyam637 <abdulsalammaryam1123@gmail.com>
2025-04-03 12:41:12 +0530

    Add files via upload

---
+++ 071_lab10_execise4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char characters[10];
+
+    printf("Enter 10 characters: ");
+    for (int i = 0; i < 10; i++) {
+        scanf(" %c", &characters[i]);
+    }
+    for (int i = 0; i < 10; i++) {
+        if (characters[i] >= 'A' && characters[i] <= 'Z') {
+            printf("%c is Uppercase\n", characters[i]);
+        } else if (characters[i] >= 'a' && characters[i] <= 'z') {
+            printf("%c is Lowercase\n", characters[i]);
+        } else {
+            printf("%c is not an alphabet\n", characters[i]);
+        }
+    }
+    return 0;
+}
---
+++ 071_lab10_exercise01.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main() {
+    int marks [5];
+    marks[0] = 98;
+
+    int ages[5] = {21,22,23,24,25};
+    ages[1] = 27;
+
+    int students_marks[5];
+    printf("Enter 5 students marks:\n");
+
+    for(int i=0; i<5; i++){
+        printf("Enter marks for student %d:",i + 1);
+        scanf("%d",&students_marks[i]);
+    }
+    printf("\nMarks of the students:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Student %d: %d\n", i + 1, students_marks[i]);
+    }
+
+    return 0;
+
+
+
+
+
+
+
+
+
+}
