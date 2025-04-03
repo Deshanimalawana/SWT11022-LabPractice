fdce5dbd5a6974e2b326a4c31ec302b739717118
Thasneemfathi <fathimathasneem200207@gmail.com>
2025-04-03 13:19:36 +0530

    Add files via upload

---
+++ labsheet8.3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int factorial(int n){
+   if(n<=1){
+            return 1;
+   }else{
+   return n * factorial(n-1);
+   }
+}
+int main (){
+    int number=5;
+    int fact=factorial(number);
+    printf("%d!=%d\n",number,fact);
+    return 0;
+
+
+}
---
+++ task8.3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int gcd(int a, int b);
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("The GCD of %d and %d is: %d\n", a, b, gcd(a, b));
+
+    return 0;
+}
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return gcd(b, a % b);
+    }
+}
