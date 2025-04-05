099b5eeb48f348a8c225c89a73ea4984493fb049
Kavindu061 <kavindujanith83@gmail.com>
2025-04-05 17:04:57 +0530

    lab10 task02.c

--- lab10 task02.c
+++ lab10 task02.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
 
 int main() {
-
-    int subjectMarks[6];
-
-
+    int marks[6];
     float average;
+    int i;
 
-
-    printf("Enter the marks for six subjects:\n");
-
-    for (int i = 0; i < 6; i++) {
-        printf("Enter mark for subject %d: ", i + 1);
-        scanf("%d", &subjectMarks[i]);
+    for (i = 0; i < 6; i++) {
+        printf("Enter Marks for student %d: ", i + 1);
+        scanf("%d", &marks[i]);
+        average += marks[i];
     }
 
+    average = average / 6.0;
 
-    int sum = 0;
-    for (int i = 0; i < 6; i++) {
-        sum += subjectMarks[i];
-    }
-    average = (float)sum / 6;
-
-
-    printf("\nMarks entered for the subjects:\n");
-    for (int i = 0; i < 6; i++) {
-        printf("Subject %d: %d\n", i + 1, subjectMarks[i]);
+    printf("Student Marks: ");
+    for (i = 0; i < 6; i++) {
+        printf("%d ", marks[i]);
     }
 
-    printf("Average of the marks: %.2f\n", average);
+    printf("\nAverage is = %.2f\n", average);
 
     return 0;
 }
