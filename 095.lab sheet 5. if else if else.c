8b1e8a4a9d0f33f3a7c0d9b5d12e06b6262de50d
Rukshana0924 <rukshlath2002@gmail.com>
2025-04-23 21:53:15 +0530

    095.lab sheet 5. if else if else.c

--- 095.lab sheet 5. if else if else.c
+++ 095.lab sheet 5. if else if else.c
@@ -4,8 +4,8 @@ int number=7;
 if (number>0){
     printf("The number is positive");
 }else if(number<0){
-printf("The number is negative");}
-else{
+printf("The number is negative");
+}else{
     printf("The number is zero");
 }
 return 0;
