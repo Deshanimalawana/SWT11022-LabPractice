a6936753de90de1a5ddba4159f3e07a490f875a3
R.M.P.M. Rathnayake <rathnayakam021@gmail.com>
2025-05-08 11:01:10 +0530

    Lab sheet 09

---
+++ Labsheet 09/Exercise 01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void displayDeveloperInfo() {
+    printf("Developed by: Pasindu\n");
+    printf("Index Number: 123\n");
+    printf("Course: Fundamentals of Programming\n");
+}
+
+int main() {
+    displayDeveloperInfo();
+    return 0;
+}
