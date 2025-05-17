0b8a7364fc013fb242d53e714a2f99531162422f
Chalana Dilshan <chalanadilshanrcc@gmail.com>
2025-05-17 08:59:17 +0530

    Update Labsheet 09 Exercise 01.c

--- 027_Lab_09/Labsheet 09 Exercise 01.c
+++ 027_Lab_09/Labsheet 09 Exercise 01.c
@@ -1,27 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-    void displayDeveloperinfo(char name[],char index[]){
-
-        printf("Developed by :%s\n",name);
-        printf("Index Number :%s\n",index);
-        printf("Course : Fundamentals of Programming\n");
-
+void displayDeveloperInfo(){
+    printf("Developed by : N.A Chalana Dilshan\n");
+    printf("Index Number : SEU/IS/22/ICT/027\n");
+    printf("Course: Fundamentals of programming\n");
 }
 
-    int main(){
-
-        char name[20],index[10];
-
-        printf("Enter Your Name:");
-        scanf("%s",name);
-
-        printf("Enter Your Index:");
-        scanf("%s",index);
-
-        printf("\n\n");
-
-        displayDeveloperinfo(name,index);
-
+int main (){
+    displayDeveloperInfo();
     return 0;
-
 }
+
+
