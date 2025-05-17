20cfbe235998488f94f151a05e67c8f78b1edd5f
vihagi-2003 <pulsarabalasoriya@gmail.com>
2025-05-17 07:57:58 -0700

    Add files via upload

---
+++ 13.3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Function to find max and min using pointers
+void findMaxMin(int a, int b, int *max, int *min) {
+    if (a > b) {
+        *max = a;
+        *min = b;
+    } else {
+        *max = b;
+        *min = a;
+    }
+}
+
+int main() {
+    int num1, num2, max, min;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    findMaxMin(num1, num2, &max, &min);
+
+    printf("Maximum: %d\n", max);
+    printf("Minimum: %d\n", min);
+
+    return 0;
+}
+
