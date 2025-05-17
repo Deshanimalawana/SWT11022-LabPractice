dfe64dad1dcadf1e19689798a61af4b87f9a8e4e
vihagi-2003 <pulsarabalasoriya@gmail.com>
2025-05-17 07:44:03 -0700

    Add files via upload

---
+++ 12.2.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main() {
+    int numbers[10];
+    int *ptr;
+    int sum = 0;
+
+    ptr = numbers;
+
+    printf("Enter 10 integers:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", ptr + i);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        sum += *(ptr + i);
+    }
+
+    printf("Sum of the 10 numbers is: %d\n", sum);
+
+    return 0;
+}
+
+
