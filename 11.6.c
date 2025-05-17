68386224fd6a93ef18c431dfebf983377f45f9d4
vihagi-2003 <pulsarabalasoriya@gmail.com>
2025-05-17 07:27:23 -0700

    Add files via upload

---
+++ 11.6.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main() {
+    int matrix1[3][2] = {{3, 6}, {7, 8}, {1, 2}};
+    int matrix2[3][2] = {{1, 9}, {6, 4}, {3, 5}};
+    int result[3][2];
+
+    // Add the matrices
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 2; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    // Display the result matrix
+    printf("Resultant Matrix:\n");
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
