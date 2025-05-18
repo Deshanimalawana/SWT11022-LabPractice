5bf21b0d9608056326fc02cb3ee66685d74ad19e
Kularasan Jelaxsi <rajanjela22@gmail.com>
2025-05-18 22:46:49 +0530

    Update 068.Labsheet 13.Exercise 02.c

--- Labsheet 13/068.Labsheet 13.Exercise 02.c
+++ Labsheet 13/068.Labsheet 13.Exercise 02.c
@@ -1,38 +1,48 @@
-#include <stdio.h>
 
-void sortArray(int *arr, int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (*(arr + i) > *(arr + j)) {
-                int temp = *(arr + i);
-                *(arr + i) = *(arr + j);
-                *(arr + j) = temp;
+  #include <stdio.h>
+
+void acceptArray(int *arr, int n) {
+    printf("Enter %d integers:\n", n);
+    for(int i = 0; i < n; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+}
+
+
+void sortArray(int *arr, int n) {
+    int temp;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+               
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-void printArray(int *arr, int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", *(arr + i));
+
+void printArray(int *arr, int n) {
+    printf("\nSorted array elements:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
 int main() {
-    int arr[100], n;
+    int n;
 
-    printf("Enter the number of elements:\n");
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    printf("Enter the elements:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
+    int arr[n]; 
 
+    acceptArray(arr, n);
     sortArray(arr, n);
-
-    printf("Sorted array: ");
     printArray(arr, n);
 
     return 0;
