4435c0c903be9dafa3e488239a1224931487f6eb
eshan <eshansaumaya@gmail.com>
2025-05-18 13:08:26 +0530

    lab week 09_035

---
+++ exercise01lb9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void displayDeveloperInfo() {
+    printf("Developed by: Eshan Saumya\n");
+    printf("Index Number: [Your Index]\n");
+    printf("Course: Fundamentals of Programming\n");
+}
+
+int main() {
+    displayDeveloperInfo();
+    return 0;
+}
