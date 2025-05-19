57cba9d5fa236489f13e2e3bbfc1fb9d77a048d9
lahiru59 <lahiru.bm08@gmail.com>
2025-05-19 17:46:59 +0530

    Add files via upload

---
+++ Labsheet-13/Exercise-02.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void acceptArray(int *arr, int size) {
+    printf("Enter %d integers:\n", size);
+    for(int i = 0; i < size; i++) {
+        scanf("%d", arr + i);
+    }
+}
+
+void sortArray(int *arr, int size) {
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i - 1; j++) {
+            if(*(arr + j) > *(arr + j + 1)) {
+                int temp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
+            }
+        }
+    }
+}
+
+void printArray(int *arr, int size) {
+    printf("Sorted array:\n");
+    for(int i = 0; i < size; i++) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+}
+
+int main() {
+    int size;
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    acceptArray(arr, size);
+    sortArray(arr, size);
+    printArray(arr, size);
+
+    return 0;
+}
