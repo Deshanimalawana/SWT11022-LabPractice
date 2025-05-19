ce676e9d43c65a42d2057a3c9ed15b06cc789e39
lahiru59 <lahiru.bm08@gmail.com>
2025-05-19 17:51:11 +0530

    Add files via upload

---
+++ LabSheet-09/exercise-01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void displayDeveloperInfo();
+
+int main() {
+    displayDeveloperInfo();
+    return 0;
+}
+
+void displayDeveloperInfo() {
+    printf("| Developed by: Lahiru Lakshan | Index Number: SEU/IS/22/ICT/059 | Course: Fundamentals of Programming |\n");
+}
---
+++ LabSheet-09/exercise-03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void greetUser();
+
+int main() {
+    greetUser();
+    return 0;
+}
+
+void greetUser() {
+    char name[50];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Hello, %s! Welcome to the lab.\n", name);
+}
