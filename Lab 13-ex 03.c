883a1df0797b989b8242d63008ed242eb64b95bb
Ironi-2003 <ironichathurika@gmail.com>
2025-05-21 03:13:40 +0530

    Add files via upload

---
+++ Lab 13-ex 03.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void findMaxMin (int num1, int num2, int *max, int *min){
+  if (num1>num2){
+    *max = num1;
+    *min = num2;
+
+  }
+  else{
+    *max = num2;
+    *min = num1;
+  }
+
+}
+
+int main (){
+   int a,b,maximum,minimum;
+   printf("Enter two Integers:");
+   scanf("%d %d", &a ,&b);
+
+   findMaxMin(a, b, &maximum, &minimum);
+
+   printf("Maximum: %d\n", maximum);
+   printf("Minimum: %d\n",minimum);
+
+   return 0;
+
+}
