865b530dbc091d499265dad50b0a3948a97be2f2
iniyavansk <iniyavanski2003@gmail.com>
2025-05-20 19:40:48 +0530

    labsheet09

---
+++ labsheet9/task01.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void DeveloperInfo();
+int main() {
+   DeveloperInfo();
+   return 0;
+}
+
+void DeveloperInfo() {
+    printf("Developed by: iniyavan\n");
+    printf("Index Number: SEU/IS/22/ICT/070\n");
+    printf("Course: Fundamentals of Programming");
+}
