ede872975240d0f8fe8c1d4b8ed344f1347e7c3f
R.M.P.M. Rathnayake <rathnayakam021@gmail.com>
2025-05-20 15:15:18 +0530

    Labsheet 12

---
+++ Labsheet 12/Exercise 01.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int int_var = 45;
+    float float_var = 57.234;
+    char char_var = 'A';
+
+    printf("Values:\n");
+    printf("int_var: %d\n", int_var);
+    printf("float_var: %.3f\n", float_var);
+    printf("char_var: %c\n", char_var);
+
+    printf("\nAddresses:\n");
+    printf("&int_var: %p\n", (void*)&int_var);
+    printf("&float_var: %p\n", (void*)&float_var);
+    printf("&char_var: %p\n", (void*)&char_var);
+
+    int *int_var_pointer = &int_var;
+    float *float_var_pointer = &float_var;
+    char *char_var_pointer = &char_var;
+
+    printf("\nPointer Values (Addresses):\n");
+    printf("int_var_pointer: %p\n", (void*)int_var_pointer);
+    printf("float_var_pointer: %p\n", (void*)float_var_pointer);
+    printf("char_var_pointer: %p\n", (void*)char_var_pointer);
+
+    printf("\nDereferenced Values:\n");
+    printf("*int_var_pointer: %d\n", *int_var_pointer);
+    printf("*float_var_pointer: %.3f\n", *float_var_pointer);
+    printf("*char_var_pointer: %c\n", *char_var_pointer);
+
+    printf("\nPointer Arithmetic:\n");
+    printf("int_var_pointer + 1: %p\n", (void*)(int_var_pointer + 1));
+    printf("float_var_pointer + 1: %p\n", (void*)(float_var_pointer + 1));
+    printf("char_var_pointer + 1: %p\n", (void*)(char_var_pointer + 1));
+
+    return 0;
+}
