7825007e360f75c8609a03e7b4995c9a2620132d
Gayanika <gayanikaishani25@gmail.com>
2025-05-23 22:28:09 +0530

    Add files via upload

---
+++ 108_lab09_Task 01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void displayDeveloperInfo() {
+    printf("Developed by: Ishani\n");
+    printf("Index Number: ICT665\n");
+    printf("Course: Fundamentals of Programming\n");
+}
+
+int main() {
+    displayDeveloperInfo();
+    return 0;
+}
