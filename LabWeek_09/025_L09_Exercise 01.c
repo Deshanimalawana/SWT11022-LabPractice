8e8270d7ba14e8d5de0b0e2734cc80869bb56707
KaveeshaSewmini <kaveesha3sewmini2003@gmail.com>
2025-05-29 11:45:12 +0530

    labsheet 09 files added

---
+++ LabWeek_09/025_L09_Exercise 01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void displayDeveloperInfo() {
+    printf("Developed by: Kaveesha\n");
+    printf("Index Number: ICT582\n");
+    printf("Course: Fundamentals of Programming\n");
+}
+
+int main() {
+    displayDeveloperInfo();
+    return 0;
+}
