429c24320da39088887ec0e4ea2f3b3c6d84cb36
Chalana Dilshan <chalanadilshanrcc@gmail.com>
2025-06-03 10:40:00 +0530

    ICT_027_Lab_12

---
+++ 027_Lab_12/Lab_12 Exercise 02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[10];
+    int *ptr;
+    int sum = 0;
+
+    ptr = numbers;
+
+    printf("Enter 10 integer numbers:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("Number %d: ", i + 1);
+        scanf("%d", ptr + i);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        sum += *(ptr + i);
+    }
+
+    printf("\nSum of the 10 numbers is: %d\n", sum);
+
+    return 0;
+}
