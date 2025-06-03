ab875fd6424eb9dbcd5210a86c224b9dab55a548
Chalana Dilshan <chalanadilshanrcc@gmail.com>
2025-06-03 11:00:29 +0530

    027_Lab_13

---
+++ 027_Lab_13/Lab_13 Exercise 01.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    int arr[3][3] = {
+        {34, 81, 96},
+        {72, 48, 24},
+        {80, 10, 71}
+    };
+
+
+    int (*ptr)[3] = arr;
+
+
+    printf("Address of the array: %p\n", (void*)ptr);
+
+
+    for (int i = 0; i < 3; i++) {
+        printf("Address of row %d: %p\n", i, (void*)(ptr + i));
+    }
+
+    printf("Addresses of each element:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Address of arr[%d][%d]: %p\n", i, j, (void*)&arr[i][j]);
+        }
+    }
+
+
+    printf("Elements of the array:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", *(*(ptr + i) + j));
+        }
+        printf("\n");
+    }
+
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            sum += *(*(ptr + i) + j);
+        }
+    }
+    printf("Sum of all elements: %d\n", sum);
+
+    return 0;
+}
