5eeb9b612579aed7ecc225477603eceaff13ef8f
NishadiTharuka2002 <tharukanishadi2023@gmail.com>
2025-06-06 18:27:32 +0530

    Add files via upload

---
+++ exercise 01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void Developerinfo();
+
+int main(){
+    Developerinfo();
+    return 0;
+}
+
+void Developerinfo(){
+    printf("Developed name :[your name] \n");
+    printf("index number : [your index]\n");
+    printf("course: fundamentals of programming");
+}
