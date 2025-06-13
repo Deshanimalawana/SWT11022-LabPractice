0d53b70a3691dd60dedb78bc97203a07bc7662c3
HirunikaRajapaksha <hirurajapaksha2003@gmail.com>
2025-06-14 00:26:33 +0530

    Add files via upload

---
+++ 094_Lab 09. Exersice 01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void displayDeveloperInfo() {
+    printf("Developed by: Hirunika\n");
+    printf("Index Number: ICT123456\n");
+    printf("Course: Fundamentals of Programming\n");
+}
+
+int main() {
+    displayDeveloperInfo();
+    return 0;
+}
