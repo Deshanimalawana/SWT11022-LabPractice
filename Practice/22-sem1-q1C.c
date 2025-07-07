65d3c680d3f7f14107271b8521148ebcf5c94e88
piragenth-satkunananthan <piragenthsatkunananthan@gmail.com>
2025-07-07 08:54:33 +0530

    file pastpaper

--- Practice/22-sem1-q1C.c
+++ Practice/22-sem1-q1C.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
-int main(){
-    int row=0,cols=0;
+int main()
+{
+    int row = 0, cols = 0;
     char space = ' ';
     char star = '*';
     printf("Enter row: ");
-    scanf("%d",&row);
+    scanf("%d", &row);
 
     printf("\nEnter cols: ");
-    scanf("%d",&cols);
+    scanf("%d", &cols);
 
-    for(int a = 0; a<=cols;a++){
-    for (int i = row ;i>=0;i--){
-            printf("%c",space*i);
-            printf("%c",star*cols);
-        }
-    }
+    for (int i = 0;)
 }
